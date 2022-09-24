9aefcd3b430bc261e19700c1d2babf39af65e6c3
Navneet Verma <navneev@amazon.com>
2022-09-23 18:01:52 -0700

    Initial Commit for setting up the neural search plugin.
    The setup includes:
        * Setting up of Github Workflows.
        * Gradle setup for the plugin.
        * Updated Readme.
        * Demo src and test classes for plugin.
    
    Signed-off-by: Navneet Verma <navneev@amazon.com>

---
+++ src/main/java/org/opensearch/neuralsearch/plugin/NeuralSearch.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.neuralsearch.plugin;
+
+import org.opensearch.plugins.Plugin;
+
+/**
+ * Neural Search plugin class
+ */
+public class NeuralSearch extends Plugin {}
---
+++ src/test/java/org/opensearch/neuralsearch/plugin/NeuralSearchIT.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.neuralsearch.plugin;
+
+import org.junit.Assert;
+import org.opensearch.test.OpenSearchIntegTestCase;
+
+public class NeuralSearchIT extends OpenSearchIntegTestCase {
+
+    /**
+     * Dummy test case for passing the build.
+     */
+    public void testDemo() {
+        Assert.assertTrue(true);
+    }
+}
