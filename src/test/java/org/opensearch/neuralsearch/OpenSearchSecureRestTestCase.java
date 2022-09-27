d623003a7b309e7a2ba2030c985029072ddb7048
Navneet Verma <navneev@amazon.com>
2022-09-27 12:45:31 -0700

    Enabled the K-NN plugin and ML plugin for integ test cluster.
    Changes include:
        * Installing K-NN and ML Plugin for running the test clusters.
        * Integration tests for validating the dependent plugins installed.
        * Enabling Integration tests for running on secure cluster.
        * Added Run and debug configurations for Plugin Testing.
    
    Signed-off-by: Navneet Verma <navneev@amazon.com>

---
+++ src/test/java/org/opensearch/neuralsearch/OpenSearchSecureRestTestCase.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.neuralsearch;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.junit.After;
+import org.opensearch.client.Request;
+import org.opensearch.client.Response;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestClientBuilder;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.unit.TimeValue;
+import org.opensearch.common.util.concurrent.ThreadContext;
+import org.opensearch.common.xcontent.DeprecationHandler;
+import org.opensearch.common.xcontent.NamedXContentRegistry;
+import org.opensearch.common.xcontent.XContentParser;
+import org.opensearch.common.xcontent.XContentType;
+import org.opensearch.test.rest.OpenSearchRestTestCase;
+
+/**
+ * Base class for running the integration tests on a secure cluster. The plugin IT test should either extend this
+ * class or create another base class by extending this class to make sure that their IT can be run on secure clusters.
+ */
+public abstract class OpenSearchSecureRestTestCase extends OpenSearchRestTestCase {
+
+    private static final String PROTOCOL_HTTP = "http";
+    private static final String PROTOCOL_HTTPS = "https";
+    private static final String SYS_PROPERTY_KEY_HTTPS = "https";
+    private static final String SYS_PROPERTY_KEY_CLUSTER_ENDPOINT = "tests.rest.cluster";
+    private static final String SYS_PROPERTY_KEY_USER = "user";
+    private static final String SYS_PROPERTY_KEY_PASSWORD = "password";
+    private static final String DEFAULT_SOCKET_TIMEOUT = "60s";
+    private static final String INTERNAL_INDICES_PREFIX = ".";
+    private static String protocol;
+
+    @Override
+    protected String getProtocol() {
+        if (protocol == null) {
+            protocol = readProtocolFromSystemProperty();
+        }
+        return protocol;
+    }
+
+    private String readProtocolFromSystemProperty() {
+        final boolean isHttps = Optional.ofNullable(System.getProperty(SYS_PROPERTY_KEY_HTTPS)).map("true"::equalsIgnoreCase).orElse(false);
+        if (!isHttps) {
+            return PROTOCOL_HTTP;
+        }
+
+        // currently only external cluster is supported for security enabled testing
+        if (Optional.ofNullable(System.getProperty(SYS_PROPERTY_KEY_CLUSTER_ENDPOINT)).isEmpty()) {
+            throw new RuntimeException("cluster url should be provided for security enabled testing");
+        }
+        return PROTOCOL_HTTPS;
+    }
+
+    @Override
+    protected RestClient buildClient(Settings settings, HttpHost[] hosts) throws IOException {
+        final RestClientBuilder builder = RestClient.builder(hosts);
+        if (PROTOCOL_HTTPS.equals(getProtocol())) {
+            configureHttpsClient(builder, settings);
+        } else {
+            configureClient(builder, settings);
+        }
+
+        return builder.build();
+    }
+
+    private void configureHttpsClient(final RestClientBuilder builder, final Settings settings) {
+        final Map<String, String> headers = ThreadContext.buildDefaultHeaders(settings);
+        final Header[] defaultHeaders = new Header[headers.size()];
+        int i = 0;
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            defaultHeaders[i++] = new BasicHeader(entry.getKey(), entry.getValue());
+        }
+        builder.setDefaultHeaders(defaultHeaders);
+        builder.setHttpClientConfigCallback(httpClientBuilder -> {
+            final String userName = Optional.ofNullable(System.getProperty(SYS_PROPERTY_KEY_USER))
+                .orElseThrow(() -> new RuntimeException("user name is missing"));
+            final String password = Optional.ofNullable(System.getProperty(SYS_PROPERTY_KEY_PASSWORD))
+                .orElseThrow(() -> new RuntimeException("password is missing"));
+            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+            credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(userName, password));
+            try {
+                return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+                    // disable the certificate since our testing cluster just uses the default security configuration
+                    .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                    .setSSLContext(SSLContextBuilder.create().loadTrustMaterial(null, (chains, authType) -> true).build());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        final String socketTimeoutString = settings.get(CLIENT_SOCKET_TIMEOUT);
+        final TimeValue socketTimeout = TimeValue.parseTimeValue(
+            socketTimeoutString == null ? DEFAULT_SOCKET_TIMEOUT : socketTimeoutString,
+            CLIENT_SOCKET_TIMEOUT
+        );
+        builder.setRequestConfigCallback(conf -> conf.setSocketTimeout(Math.toIntExact(socketTimeout.getMillis())));
+        if (settings.hasValue(CLIENT_PATH_PREFIX)) {
+            builder.setPathPrefix(settings.get(CLIENT_PATH_PREFIX));
+        }
+    }
+
+    /**
+     * wipeAllIndices won't work since it cannot delete security index. Use deleteExternalIndices instead.
+     */
+    @Override
+    protected boolean preserveIndicesUponCompletion() {
+        return true;
+    }
+
+    @After
+    public void deleteExternalIndices() throws IOException {
+        final Response response = client().performRequest(new Request("GET", "/_cat/indices?format=json" + "&expand_wildcards=all"));
+        final XContentType xContentType = XContentType.fromMediaType(response.getEntity().getContentType().getValue());
+        try (
+            final XContentParser parser = xContentType.xContent()
+                .createParser(
+                    NamedXContentRegistry.EMPTY,
+                    DeprecationHandler.THROW_UNSUPPORTED_OPERATION,
+                    response.getEntity().getContent()
+                )
+        ) {
+            final XContentParser.Token token = parser.nextToken();
+            final List<Map<String, Object>> parserList;
+            if (token == XContentParser.Token.START_ARRAY) {
+                parserList = parser.listOrderedMap().stream().map(obj -> (Map<String, Object>) obj).collect(Collectors.toList());
+            } else {
+                parserList = Collections.singletonList(parser.mapOrdered());
+            }
+
+            final List<String> externalIndices = parserList.stream()
+                .map(index -> (String) index.get("index"))
+                .filter(indexName -> indexName != null)
+                .filter(indexName -> !indexName.startsWith(INTERNAL_INDICES_PREFIX))
+                .collect(Collectors.toList());
+
+            for (final String indexName : externalIndices) {
+                adminClient().performRequest(new Request("DELETE", "/" + indexName));
+            }
+        }
+    }
+}
---
+++ src/test/java/org/opensearch/neuralsearch/plugin/ValidateDependentPluginInstallationIT.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.neuralsearch.plugin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.opensearch.client.Request;
+import org.opensearch.client.Response;
+import org.opensearch.common.Strings;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.xcontent.XContentFactory;
+import org.opensearch.neuralsearch.OpenSearchSecureRestTestCase;
+import org.opensearch.rest.RestRequest;
+
+/**
+ * This test validates that dependent plugin defined in build.gradle -> opensearchplugin.extendedPlugins are
+ * installed or not. We don't want to run these tests before every IT as it can slow down the execution of IT. Hence
+ * doing it separately.
+ */
+public class ValidateDependentPluginInstallationIT extends OpenSearchSecureRestTestCase {
+
+    private static final String KNN_INDEX_NAME = "neuralsearchknnindexforvalidation";
+    private static final String KNN_VECTOR_FIELD_NAME = "vectorField";
+    private static final Set<String> DEPENDENT_PLUGINS = Set.of("opensearch-ml", "opensearch-knn");
+    private static final String GET_PLUGINS_URL = "_cat/plugins";
+    private static final String ML_PLUGIN_STATS_URL = "_plugins/_ml/stats";
+    private static final String KNN_DOCUMENT_URL = KNN_INDEX_NAME + "/_doc/1?refresh";
+
+    public void testDependentPluginsInstalled() throws IOException {
+        final Set<String> installedPlugins = getAllInstalledPlugins();
+        Assert.assertTrue(installedPlugins.containsAll(DEPENDENT_PLUGINS));
+    }
+
+    /**
+     * Validate K-NN Plugin Setup by creating a k-NN index and then deleting the index. Not adding the index deletion
+     * in the cleanup setup as index creation was not part of whole setup for this test cases class.
+     */
+    public void testValidateKNNPluginSetup() throws IOException {
+        createBasicKnnIndex();
+        Assert.assertTrue(indexExists(KNN_INDEX_NAME));
+        indexDocument();
+        getDocument();
+        deleteIndex(KNN_INDEX_NAME);
+        Assert.assertFalse(indexExists(KNN_INDEX_NAME));
+    }
+
+    /**
+     * Validate ML-Plugin setup. We are using the stats API of ML Plugin to validate plugin is present or not. This
+     * was the best API that we can use without creating any side effects.
+     */
+    public void testValidateMLPluginSetup() throws IOException {
+        final Request request = new Request(RestRequest.Method.GET.name(), ML_PLUGIN_STATS_URL);
+        assertOK(client().performRequest(request));
+    }
+
+    private void createBasicKnnIndex() throws IOException {
+        String mapping = Strings.toString(
+            XContentFactory.jsonBuilder()
+                .startObject()
+                .startObject("properties")
+                .startObject(KNN_VECTOR_FIELD_NAME)
+                .field("type", "knn_vector")
+                .field("dimension", Integer.toString(3))
+                .startObject("method")
+                .field("engine", "lucene")
+                .field("name", "hnsw")
+                .endObject()
+                .endObject()
+                .endObject()
+                .endObject()
+        );
+        mapping = mapping.substring(1, mapping.length() - 1);
+        createIndex(KNN_INDEX_NAME, Settings.EMPTY, mapping);
+    }
+
+    private Set<String> getAllInstalledPlugins() throws IOException {
+        final Request request = new Request(RestRequest.Method.GET.name(), GET_PLUGINS_URL);
+        final Response response = client().performRequest(request);
+        assertOK(response);
+        final BufferedReader responseReader = new BufferedReader(
+            new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8)
+        );
+        final Set<String> installedPluginsSet = new HashSet<>();
+        String line;
+        while ((line = responseReader.readLine()) != null) {
+            // Output looks like this: integTest-0 opensearch-knn 2.3.0.0
+            final String pluginName = line.split("\\s+")[1];
+            installedPluginsSet.add(pluginName);
+        }
+        return installedPluginsSet;
+    }
+
+    private void indexDocument() throws IOException {
+        final String indexRequestBody = Strings.toString(
+            XContentFactory.jsonBuilder()
+                .startObject()
+                .startArray(KNN_VECTOR_FIELD_NAME)
+                .value(1.0)
+                .value(2.0)
+                .value(4.0)
+                .endArray()
+                .endObject()
+        );
+        final Request indexRequest = new Request(RestRequest.Method.POST.name(), KNN_DOCUMENT_URL);
+        indexRequest.setJsonEntity(indexRequestBody);
+        assertOK(client().performRequest(indexRequest));
+    }
+
+    private void getDocument() throws IOException {
+        final Request getDocumentRequest = new Request(RestRequest.Method.GET.name(), KNN_DOCUMENT_URL);
+        assertOK(client().performRequest(getDocumentRequest));
+    }
+}
