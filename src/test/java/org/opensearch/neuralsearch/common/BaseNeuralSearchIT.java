eadd47a81cc08a550227a7a6607f971ae975b4cd
Zan Niu <zaniu@amazon.com>
2022-10-13 20:12:33 +0800

    Add BaseNeuralSearchIT and address other review comments
    Signed-off-by: Zan Niu <zaniu@amazon.com>

--- src/test/java/org/opensearch/neuralsearch/common/BaseNeuralSearchIT.java
+++ src/test/java/org/opensearch/neuralsearch/common/BaseNeuralSearchIT.java
@@ -5,15 +5,21 @@
 
 package org.opensearch.neuralsearch.common;
 
+import static org.apache.http.entity.ContentType.APPLICATION_JSON;
+
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.function.Predicate;
 
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.util.EntityUtils;
-import org.opensearch.client.Response;
-import org.opensearch.neuralsearch.utils.TestHelper;
+import org.opensearch.client.*;
 import org.opensearch.test.rest.OpenSearchRestTestCase;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -27,12 +33,12 @@ public abstract class BaseNeuralSearchIT extends OpenSearchRestTestCase {
     private static final Locale locale = Locale.getDefault();
 
     public String uploadModel(String requestBody) throws Exception {
-        Response uploadResponse = TestHelper.makeRequest(
+        Response uploadResponse = makeRequest(
             client(),
             "POST",
             "/_plugins/_ml/models/_upload",
             null,
-            TestHelper.toHttpEntity(requestBody),
+            toHttpEntity(requestBody),
             ImmutableList.of(new BasicHeader(HttpHeaders.USER_AGENT, "Kibana"))
         );
         JsonNode uploadResJson = objectMapper.readTree(EntityUtils.toString(uploadResponse.getEntity()));
@@ -51,12 +57,12 @@ public abstract class BaseNeuralSearchIT extends OpenSearchRestTestCase {
     }
 
     public void loadModel(String modelId) throws IOException {
-        Response uploadResponse = TestHelper.makeRequest(
+        Response uploadResponse = makeRequest(
             client(),
             "POST",
             String.format(locale, "/_plugins/_ml/models/%s/_load", modelId),
             null,
-            TestHelper.toHttpEntity(""),
+            toHttpEntity(""),
             ImmutableList.of(new BasicHeader(HttpHeaders.USER_AGENT, "Kibana"))
         );
         JsonNode uploadResJson = objectMapper.readTree(EntityUtils.toString(uploadResponse.getEntity()));
@@ -72,12 +78,12 @@ public abstract class BaseNeuralSearchIT extends OpenSearchRestTestCase {
     }
 
     public JsonNode getTaskQueryResponse(String taskId) throws IOException {
-        Response taskQueryResponse = TestHelper.makeRequest(
+        Response taskQueryResponse = makeRequest(
             client(),
             "GET",
             String.format(locale, "_plugins/_ml/tasks/%s", taskId),
             null,
-            TestHelper.toHttpEntity(""),
+            toHttpEntity(""),
             ImmutableList.of(new BasicHeader(HttpHeaders.USER_AGENT, "Kibana"))
         );
         return objectMapper.readTree(EntityUtils.toString(taskQueryResponse.getEntity()));
@@ -86,6 +92,48 @@ public abstract class BaseNeuralSearchIT extends OpenSearchRestTestCase {
     public boolean checkComplete(JsonNode node) {
         Predicate<JsonNode> predicate = x -> node.get("error") != null || "COMPLETED".equals(node.get("state").asText());
         return predicate.test(node);
+    }
+
+    public static Response makeRequest(
+        RestClient client,
+        String method,
+        String endpoint,
+        Map<String, String> params,
+        HttpEntity entity,
+        List<Header> headers
+    ) throws IOException {
+        return makeRequest(client, method, endpoint, params, entity, headers, false);
+    }
+
+    public static Response makeRequest(
+        RestClient client,
+        String method,
+        String endpoint,
+        Map<String, String> params,
+        HttpEntity entity,
+        List<Header> headers,
+        boolean strictDeprecationMode
+    ) throws IOException {
+        Request request = new Request(method, endpoint);
+
+        RequestOptions.Builder options = RequestOptions.DEFAULT.toBuilder();
+        if (headers != null) {
+            headers.forEach(header -> options.addHeader(header.getName(), header.getValue()));
+        }
+        options.setWarningsHandler(strictDeprecationMode ? WarningsHandler.STRICT : WarningsHandler.PERMISSIVE);
+        request.setOptions(options.build());
+
+        if (params != null) {
+            params.entrySet().forEach(it -> request.addParameter(it.getKey(), it.getValue()));
+        }
+        if (entity != null) {
+            request.setEntity(entity);
+        }
+        return client.performRequest(request);
+    }
+
+    public static HttpEntity toHttpEntity(String jsonString) {
+        return new StringEntity(jsonString, APPLICATION_JSON);
     }
 
 }
--- src/test/java/org/opensearch/neuralsearch/utils/TestHelper.java
+++ src/test/java/org/opensearch/neuralsearch/utils/TestHelper.java
@@ -5,58 +5,6 @@
 
 package org.opensearch.neuralsearch.utils;
 
-import static org.apache.http.entity.ContentType.APPLICATION_JSON;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.entity.StringEntity;
-import org.opensearch.client.*;
-
 public class TestHelper {
 
-    public static Response makeRequest(
-        RestClient client,
-        String method,
-        String endpoint,
-        Map<String, String> params,
-        HttpEntity entity,
-        List<Header> headers
-    ) throws IOException {
-        return makeRequest(client, method, endpoint, params, entity, headers, false);
-    }
-
-    public static Response makeRequest(
-        RestClient client,
-        String method,
-        String endpoint,
-        Map<String, String> params,
-        HttpEntity entity,
-        List<Header> headers,
-        boolean strictDeprecationMode
-    ) throws IOException {
-        Request request = new Request(method, endpoint);
-
-        RequestOptions.Builder options = RequestOptions.DEFAULT.toBuilder();
-        if (headers != null) {
-            headers.forEach(header -> options.addHeader(header.getName(), header.getValue()));
-        }
-        options.setWarningsHandler(strictDeprecationMode ? WarningsHandler.STRICT : WarningsHandler.PERMISSIVE);
-        request.setOptions(options.build());
-
-        if (params != null) {
-            params.entrySet().forEach(it -> request.addParameter(it.getKey(), it.getValue()));
-        }
-        if (entity != null) {
-            request.setEntity(entity);
-        }
-        return client.performRequest(request);
-    }
-
-    public static HttpEntity toHttpEntity(String jsonString) {
-        return new StringEntity(jsonString, APPLICATION_JSON);
-    }
 }
