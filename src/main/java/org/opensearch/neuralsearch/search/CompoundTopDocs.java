7dda0c58f2006ca0b140e5dd5fafe6b0fbad5d02
Martin Gaievski <gaievski@amazon.com>
2023-06-30 00:16:51 +0200

    Add Query phase searcher (#204)
    * Add query phase searcher and basic tests
    
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>
    
    ---------
    
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/search/CompoundTopDocs.java
+++ src/main/java/org/opensearch/neuralsearch/search/CompoundTopDocs.java
@@ -6,6 +6,7 @@
 package org.opensearch.neuralsearch.search;
 
 import java.util.Arrays;
+import java.util.List;
 
 import lombok.Getter;
 import lombok.ToString;
@@ -21,23 +22,23 @@ import org.apache.lucene.search.TotalHits;
 public class CompoundTopDocs extends TopDocs {
 
     @Getter
-    private TopDocs[] compoundTopDocs;
+    private List<TopDocs> compoundTopDocs;
 
     public CompoundTopDocs(TotalHits totalHits, ScoreDoc[] scoreDocs) {
         super(totalHits, scoreDocs);
     }
 
-    public CompoundTopDocs(TotalHits totalHits, TopDocs[] docs) {
+    public CompoundTopDocs(TotalHits totalHits, List<TopDocs> docs) {
         // we pass clone of score docs from the sub-query that has most hits
         super(totalHits, cloneLargestScoreDocs(docs));
         this.compoundTopDocs = docs;
     }
 
-    private static ScoreDoc[] cloneLargestScoreDocs(TopDocs[] docs) {
+    private static ScoreDoc[] cloneLargestScoreDocs(List<TopDocs> docs) {
         if (docs == null) {
             return null;
         }
-        ScoreDoc[] maxScoreDocs = null;
+        ScoreDoc[] maxScoreDocs = new ScoreDoc[0];
         int maxLength = -1;
         for (TopDocs topDoc : docs) {
             if (topDoc == null || topDoc.scoreDocs == null) {
@@ -47,9 +48,6 @@ public class CompoundTopDocs extends TopDocs {
                 maxLength = topDoc.scoreDocs.length;
                 maxScoreDocs = topDoc.scoreDocs;
             }
-        }
-        if (maxScoreDocs == null) {
-            return null;
         }
         // do deep copy
         return Arrays.stream(maxScoreDocs).map(doc -> new ScoreDoc(doc.doc, doc.score, doc.shardIndex)).toArray(ScoreDoc[]::new);
