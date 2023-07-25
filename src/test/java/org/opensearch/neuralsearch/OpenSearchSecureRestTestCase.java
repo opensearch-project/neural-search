1066c35d91822062298d4ef2ed7f3a62a9a93296
Martin Gaievski <gaievski@amazon.com>
2023-07-24 17:21:07 -0700

    Switch from XContentType to MediaType to fix compilation errors
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>

--- src/test/java/org/opensearch/neuralsearch/OpenSearchSecureRestTestCase.java
+++ src/test/java/org/opensearch/neuralsearch/OpenSearchSecureRestTestCase.java
@@ -36,8 +36,8 @@ import org.opensearch.client.RestClientBuilder;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.unit.TimeValue;
 import org.opensearch.common.util.concurrent.ThreadContext;
-import org.opensearch.common.xcontent.XContentType;
 import org.opensearch.core.xcontent.DeprecationHandler;
+import org.opensearch.core.xcontent.MediaType;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
 import org.opensearch.core.xcontent.XContentParser;
 import org.opensearch.test.rest.OpenSearchRestTestCase;
@@ -150,7 +150,7 @@ public abstract class OpenSearchSecureRestTestCase extends OpenSearchRestTestCas
     @After
     public void deleteExternalIndices() throws IOException {
         final Response response = client().performRequest(new Request("GET", "/_cat/indices?format=json" + "&expand_wildcards=all"));
-        final XContentType xContentType = XContentType.fromMediaType(response.getEntity().getContentType());
+        final MediaType xContentType = MediaType.fromMediaType(response.getEntity().getContentType());
         try (
             final XContentParser parser = xContentType.xContent()
                 .createParser(
