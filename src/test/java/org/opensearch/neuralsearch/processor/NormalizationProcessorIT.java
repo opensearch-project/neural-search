e250a85975a38c12be490890ea4c73fe5241c4a7
Martin Gaievski <gaievski@amazon.com>
2023-08-29 23:19:18 +0200

    Changed format for hybrid query results to a single list of scores with delimiter (#259)
    * Changed approach for storing hybrid query results from compound top docs to signle list of scores with delimiter
    
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>
    (cherry picked from commit 75b59cd5f0f45c83f7e6d970f9ecc218d84cdcd7)

--- src/test/java/org/opensearch/neuralsearch/processor/NormalizationProcessorIT.java
+++ src/test/java/org/opensearch/neuralsearch/processor/NormalizationProcessorIT.java
@@ -42,8 +42,11 @@ public class NormalizationProcessorIT extends BaseNeuralSearchIT {
     private static final String TEST_DOC_TEXT3 = "We would like to welcome everyone";
     private static final String TEST_DOC_TEXT4 = "Hello, I'm glad to you see you pal";
     private static final String TEST_DOC_TEXT5 = "Say hello and enter my friend";
+    private static final String TEST_DOC_TEXT6 = "This tale grew in the telling";
+    private static final String TEST_DOC_TEXT7 = "They do not and did not understand or like machines";
     private static final String TEST_KNN_VECTOR_FIELD_NAME_1 = "test-knn-vector-1";
     private static final String TEST_TEXT_FIELD_NAME_1 = "test-text-field-1";
+    private static final String TEST_TEXT_FIELD_NAME_2 = "test-text-field-2";
     private static final int TEST_DIMENSION = 768;
     private static final SpaceType TEST_SPACE_TYPE = SpaceType.L2;
     private static final String SEARCH_PIPELINE = "phase-results-pipeline";
@@ -172,7 +175,7 @@ public class NormalizationProcessorIT extends BaseNeuralSearchIT {
         assertNotNull(total.get("relation"));
         assertEquals(RELATION_EQUAL_TO, total.get("relation"));
         assertTrue(getMaxScore(searchResponseAsMap).isPresent());
-        assertTrue(Range.between(.75f, 1.0f).contains(getMaxScore(searchResponseAsMap).get()));
+        assertTrue(Range.between(.5f, 1.0f).contains(getMaxScore(searchResponseAsMap).get()));
         List<Map<String, Object>> hitsNestedList = getNestedHits(searchResponseAsMap);
         List<String> ids = new ArrayList<>();
         List<Double> scores = new ArrayList<>();
@@ -187,7 +190,7 @@ public class NormalizationProcessorIT extends BaseNeuralSearchIT {
         // based on random vectors and return results for every doc. In some cases that may affect 1.0 score from term query and make it
         // lower.
         float highestScore = scores.stream().max(Double::compare).get().floatValue();
-        assertTrue(Range.between(.75f, 1.0f).contains(highestScore));
+        assertTrue(Range.between(.5f, 1.0f).contains(highestScore));
         float lowestScore = scores.stream().min(Double::compare).get().floatValue();
         assertTrue(Range.between(.0f, .5f).contains(lowestScore));
 
@@ -231,7 +234,7 @@ public class NormalizationProcessorIT extends BaseNeuralSearchIT {
             5,
             Map.of("search_pipeline", SEARCH_PIPELINE)
         );
-        assertQueryResults(searchResponseAsMap, 4, true);
+        assertQueryResults(searchResponseAsMap, 4, true, Range.between(0.33f, 1.0f));
     }
 
     private void initializeIndexIfNotExist(String indexName) throws IOException {
@@ -293,8 +296,8 @@ public class NormalizationProcessorIT extends BaseNeuralSearchIT {
                 "1",
                 Collections.singletonList(TEST_KNN_VECTOR_FIELD_NAME_1),
                 Collections.singletonList(Floats.asList(testVector1).toArray()),
-                Collections.singletonList(TEST_TEXT_FIELD_NAME_1),
-                Collections.singletonList(TEST_DOC_TEXT1)
+                List.of(TEST_TEXT_FIELD_NAME_1, TEST_TEXT_FIELD_NAME_2),
+                List.of(TEST_DOC_TEXT1, TEST_DOC_TEXT6)
             );
             addKnnDoc(
                 TEST_MULTI_DOC_INDEX_THREE_SHARDS_NAME,
@@ -307,8 +310,8 @@ public class NormalizationProcessorIT extends BaseNeuralSearchIT {
                 "3",
                 Collections.singletonList(TEST_KNN_VECTOR_FIELD_NAME_1),
                 Collections.singletonList(Floats.asList(testVector3).toArray()),
-                Collections.singletonList(TEST_TEXT_FIELD_NAME_1),
-                Collections.singletonList(TEST_DOC_TEXT2)
+                List.of(TEST_TEXT_FIELD_NAME_1, TEST_TEXT_FIELD_NAME_2),
+                List.of(TEST_DOC_TEXT2, TEST_DOC_TEXT7)
             );
             addKnnDoc(
                 TEST_MULTI_DOC_INDEX_THREE_SHARDS_NAME,
@@ -354,6 +357,15 @@ public class NormalizationProcessorIT extends BaseNeuralSearchIT {
     }
 
     private void assertQueryResults(Map<String, Object> searchResponseAsMap, int totalExpectedDocQty, boolean assertMinScore) {
+        assertQueryResults(searchResponseAsMap, totalExpectedDocQty, assertMinScore, Range.between(0.5f, 1.0f));
+    }
+
+    private void assertQueryResults(
+        Map<String, Object> searchResponseAsMap,
+        int totalExpectedDocQty,
+        boolean assertMinScore,
+        Range<Float> maxScoreRange
+    ) {
         assertNotNull(searchResponseAsMap);
         Map<String, Object> total = getTotalHits(searchResponseAsMap);
         assertNotNull(total.get("value"));
@@ -362,7 +374,7 @@ public class NormalizationProcessorIT extends BaseNeuralSearchIT {
         assertEquals(RELATION_EQUAL_TO, total.get("relation"));
         assertTrue(getMaxScore(searchResponseAsMap).isPresent());
         if (totalExpectedDocQty > 0) {
-            assertEquals(1.0, getMaxScore(searchResponseAsMap).get(), 0.001f);
+            assertTrue(maxScoreRange.contains(getMaxScore(searchResponseAsMap).get()));
         } else {
             assertEquals(0.0, getMaxScore(searchResponseAsMap).get(), 0.001f);
         }
@@ -378,7 +390,7 @@ public class NormalizationProcessorIT extends BaseNeuralSearchIT {
         assertTrue(IntStream.range(0, scores.size() - 1).noneMatch(idx -> scores.get(idx) < scores.get(idx + 1)));
         // verify the scores are normalized
         if (totalExpectedDocQty > 0) {
-            assertEquals(1.0, (double) scores.stream().max(Double::compare).get(), 0.001);
+            assertTrue(maxScoreRange.contains(scores.stream().max(Double::compare).get().floatValue()));
             if (assertMinScore) {
                 assertEquals(0.001, (double) scores.stream().min(Double::compare).get(), 0.001);
             }
