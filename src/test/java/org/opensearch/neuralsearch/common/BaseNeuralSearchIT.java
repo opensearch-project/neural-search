5c0f9acc0439516f2047d7a4d91b7c0a84ab10c8
zhichao-aws <zhichaog@amazon.com>
2023-09-27 14:38:58 +0800

    add 3s sleep before delete model
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>

--- src/test/java/org/opensearch/neuralsearch/common/BaseNeuralSearchIT.java
+++ src/test/java/org/opensearch/neuralsearch/common/BaseNeuralSearchIT.java
@@ -550,6 +550,13 @@ public abstract class BaseNeuralSearchIT extends OpenSearchSecureRestTestCase {
             ImmutableList.of(new BasicHeader(HttpHeaders.USER_AGENT, DEFAULT_USER_AGENT))
         );
 
+        // after model undeploy returns, the max interval to update model status is 3s in ml-commons CronJob.
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         makeRequest(
             client(),
             "DELETE",
