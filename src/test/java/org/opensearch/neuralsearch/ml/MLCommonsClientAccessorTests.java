06dfe4be7dcae2606b49200dfe924398dcbb5ba3
zhichao-aws <zhichaog@amazon.com>
2023-09-27 06:27:21 +0000

    add import
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>

--- src/test/java/org/opensearch/neuralsearch/ml/MLCommonsClientAccessorTests.java
+++ src/test/java/org/opensearch/neuralsearch/ml/MLCommonsClientAccessorTests.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.mockito.ArgumentCaptor;
