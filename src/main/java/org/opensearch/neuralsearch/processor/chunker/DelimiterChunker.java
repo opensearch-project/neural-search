41cff0c83c093065ae0700057a87ba8bf71257ed
xinyual <xinyual@amazon.com>
2024-02-26 14:34:39 +0800

    add delimiter chunker
    Signed-off-by: xinyual <xinyual@amazon.com>
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/processor/chunker/DelimiterChunker.java
+++ src/main/java/org/opensearch/neuralsearch/processor/chunker/DelimiterChunker.java
@@ -4,20 +4,42 @@
  */
 package org.opensearch.neuralsearch.processor.chunker;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class DelimiterChunker implements IFieldChunker {
 
     public DelimiterChunker() {}
 
+    public static String DELIMITER_FIELD = "delimiter";
+
+
     @Override
     public void validateParameters(Map<String, Object> parameters) {
-        throw new UnsupportedOperationException("delimiter chunker has not been implemented yet");
+        if (parameters.containsKey(DELIMITER_FIELD))
+        {
+            Object delimiter = parameters.get(DELIMITER_FIELD);
+            if (!(delimiter instanceof String)){
+                throw new IllegalArgumentException("delimiter parameters " + delimiter + " must be string");
+            }
+        }
+        else {
+            throw new IllegalArgumentException("You must contain field:" + DELIMITER_FIELD + " in your parameter");
+        }
     }
 
     @Override
     public List<String> chunk(String content, Map<String, Object> parameters) {
-        throw new UnsupportedOperationException("delimiter chunker has not been implemented yet");
+        List<String > chunkingResult = new ArrayList<>();
+        String delimiter = (String) parameters.get(DELIMITER_FIELD);
+        Scanner scanner = new Scanner(content);
+        scanner.useDelimiter(delimiter);
+        while (scanner.hasNext()) {
+            String nextChunk = scanner.next();
+            chunkingResult.add(nextChunk);
+        }
+        return chunkingResult;
     }
 }
