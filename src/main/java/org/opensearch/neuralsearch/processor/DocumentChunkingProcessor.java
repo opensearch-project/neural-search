2f23c303a9aadf4a7055ca447e2ad776fabd9c95
yuye-aws <yuyezhu@amazon.com>
2024-02-26 19:21:25 +0800

    remove system out println
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/processor/DocumentChunkingProcessor.java
+++ src/main/java/org/opensearch/neuralsearch/processor/DocumentChunkingProcessor.java
@@ -168,7 +168,6 @@ public final class DocumentChunkingProcessor extends AbstractProcessor {
 
     private Object chunk(IFieldChunker chunker, Object content, Map<String, Object> chunkerParameters) {
         // assume that content is either a map, list or string
-        System.out.println("chunkerParameters: " + chunkerParameters);
         if (content instanceof Map) {
             Map<String, Object> chunkedPassageMap = new HashMap<>();
             Map<String, Object> contentMap = (Map<String, Object>) content;
