ae882f44d7d7e90bad9c86f4cb9e2937e4e5464d
zhichao-aws <zhichaog@amazon.com>
2024-03-06 17:20:53 +0800

    add it for bwc restart-upgrade
    Signed-off-by: zhichao-aws <zhichaog@amazon.com>

--- qa/restart-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/NeuralQueryEnricherProcessorIT.java
+++ qa/restart-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/NeuralQueryEnricherProcessorIT.java
@@ -16,7 +16,6 @@ import org.opensearch.neuralsearch.query.NeuralSparseQueryBuilder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
 
 public class NeuralQueryEnricherProcessorIT extends AbstractRestartUpgradeRestTestCase {
     private static final String SPARSE_INGEST_PIPELINE_NAME = "nlp-ingest-pipeline-sparse";
@@ -25,21 +24,16 @@ public class NeuralQueryEnricherProcessorIT extends AbstractRestartUpgradeRestTe
     private static final String TEST_TEXT_FIELD = "passage_text";
     private static final String TEXT_1 = "Hello world a b";
     private static final String TEXT_2 = "Hello planet";
-    private static final List<String> TEST_TOKENS_1 = List.of("hello", "world", "a", "b", "c");
-    private static final List<String> TEST_TOKENS_2 = List.of("hello", "planet", "a", "b", "c");
-    private final Map<String, Float> testRankFeaturesDoc1 = TestUtils.createRandomTokenWeightMap(TEST_TOKENS_1);
-    private final Map<String, Float> testRankFeaturesDoc2 = TestUtils.createRandomTokenWeightMap(TEST_TOKENS_2);
 
-    // Test restart-upgrade test sparse embedding processor
-    // Create Sparse Encoding Processor, Ingestion Pipeline and add document
-    // Validate process , pipeline and document count in restart-upgrade scenario
+    // Test restart-upgrade neural_query_enricher in restart-upgrade scenario
     public void testNeuralQueryEnricherProcessor_NeuralSparseSearch_E2EFlow() throws Exception {
         waitForClusterHealthGreen(NODES_BWC_CLUSTER);
         Version bwcVersion = parseVersionFromString(getBWCVersion().get());
         logger.info("bwc version: " + bwcVersion.toString());
 
         if (isRunningAgainstOldCluster()) {
-            String modelId = prepareSparseEncodingModel();
+            String modelId = uploadSparseEncodingModel();
+            loadModel(modelId);
             createPipelineForSparseEncodingProcessor(modelId, SPARSE_INGEST_PIPELINE_NAME);
             createIndexWithConfiguration(
                 getIndexNameForTest(),
@@ -47,22 +41,20 @@ public class NeuralQueryEnricherProcessorIT extends AbstractRestartUpgradeRestTe
                 SPARSE_INGEST_PIPELINE_NAME
             );
 
-            addSparseEncodingDoc(
-                getIndexNameForTest(),
-                "0",
-                List.of(),
-                List.of(),
-                List.of(TEST_TEXT_FIELD),
-                List.of(TEXT_1)
-            );
+            addSparseEncodingDoc(getIndexNameForTest(), "0", List.of(), List.of(), List.of(TEST_TEXT_FIELD), List.of(TEXT_1));
 
             createSearchRequestProcessor(modelId, SPARSE_SEARCH_PIPELINE_NAME);
-            updateIndexSettings(getIndexNameForTest(), Settings.builder().put("index.search.default_pipeline", SPARSE_SEARCH_PIPELINE_NAME));
+            updateIndexSettings(
+                getIndexNameForTest(),
+                Settings.builder().put("index.search.default_pipeline", SPARSE_SEARCH_PIPELINE_NAME)
+            );
             if (bwcVersion.onOrAfter(Version.V_2_13_0)) {
                 // after we support default model id in neural_sparse query
                 // do nothing here. need to add test codes after finishing backport
                 ;
             } else {
+                // deploy model
+                loadModel(modelId);
                 // before we support default model id in neural_sparse query
                 NeuralSparseQueryBuilder sparseEncodingQueryBuilderWithoutModelId = new NeuralSparseQueryBuilder().fieldName(
                     TEST_SPARSE_ENCODING_FIELD
