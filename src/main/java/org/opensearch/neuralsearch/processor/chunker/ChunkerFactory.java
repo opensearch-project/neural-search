d05b2469201c4f665d473db429360138c39328ef
xinyual <xinyual@amazon.com>
2024-03-11 13:45:33 +0800

    fix chunker factory
    Signed-off-by: xinyual <xinyual@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/processor/chunker/ChunkerFactory.java
+++ src/main/java/org/opensearch/neuralsearch/processor/chunker/ChunkerFactory.java
@@ -7,6 +7,8 @@ package org.opensearch.neuralsearch.processor.chunker;
 import java.util.Map;
 import java.util.Set;
 
+import org.opensearch.index.analysis.AnalysisRegistry;
+
 /**
  * A factory to create different chunking algorithm classes and return all supported chunking algorithms.
  */
@@ -15,15 +17,15 @@ public class ChunkerFactory {
     public static final String FIXED_TOKEN_LENGTH_ALGORITHM = "fixed_token_length";
     public static final String DELIMITER_ALGORITHM = "delimiter";
 
-    public static Chunker create(String type, Map<String, Object> parameters) {
+    public static Chunker create(String type, AnalysisRegistry analysisRegistry, Map<String, Object> parameters) {
         switch (type) {
             case FIXED_TOKEN_LENGTH_ALGORITHM:
-                return new FixedTokenLengthChunker(parameters);
+                return new FixedTokenLengthChunker(analysisRegistry, parameters);
             case DELIMITER_ALGORITHM:
                 return new DelimiterChunker(parameters);
             default:
                 throw new IllegalArgumentException(
-                    "chunker type [" + type + "] is not supported. Supported chunkers types are " + ChunkerFactory.getAllChunkers()
+                        "chunker type [" + type + "] is not supported. Supported chunkers types are " + ChunkerFactory.getAllChunkers()
                 );
         }
     }
