0f9c140f8812d47a878563d10388228bc796659f
yuye-aws <yuyezhu@amazon.com>
2024-03-14 12:55:22 +0800

    implement a map from chunker name to constuctor function in chunker factory
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/processor/chunker/ChunkerFactory.java
+++ src/main/java/org/opensearch/neuralsearch/processor/chunker/ChunkerFactory.java
@@ -4,34 +4,36 @@
  */
 package org.opensearch.neuralsearch.processor.chunker;
 
+import com.google.common.collect.ImmutableMap;
+
 import java.util.Map;
 import java.util.Set;
 import java.util.Locale;
+import java.util.function.Function;
 
 /**
  * A factory to create different chunking algorithm objects and return all supported chunking algorithms.
  */
 public class ChunkerFactory {
 
+    private static final ImmutableMap<String, Function<Map<String, Object>, Chunker>> chunkers = ImmutableMap.of(
+        FixedTokenLengthChunker.ALGORITHM_NAME,
+        FixedTokenLengthChunker::new,
+        DelimiterChunker.ALGORITHM_NAME,
+        FixedTokenLengthChunker::new
+    );
+
     public static Chunker create(final String type, final Map<String, Object> parameters) {
-        switch (type) {
-            case FixedTokenLengthChunker.ALGORITHM_NAME:
-                return new FixedTokenLengthChunker(parameters);
-            case DelimiterChunker.ALGORITHM_NAME:
-                return new DelimiterChunker(parameters);
-            default:
-                throw new IllegalArgumentException(
-                    String.format(
-                        Locale.ROOT,
-                        "chunking algorithm [%s] is not supported. Supported chunking algorithms are %s",
-                        type,
-                        ChunkerFactory.getAllChunkers()
-                    )
-                );
+        Function<Map<String, Object>, Chunker> chunkerConstructionFunction= chunkers.get(type);
+        if (chunkerConstructionFunction == null) {
+            throw new IllegalArgumentException(
+                String.format(Locale.ROOT, "chunking algorithm [%s] is not supported. Supported chunking algorithms are %s", type, chunkers.keySet())
+            );
         }
+        return chunkerConstructionFunction.apply(parameters);
     }
 
     public static Set<String> getAllChunkers() {
-        return Set.of(FixedTokenLengthChunker.ALGORITHM_NAME, DelimiterChunker.ALGORITHM_NAME);
+        return chunkers.keySet();
     }
 }
