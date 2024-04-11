821dee8ed692aac15196ebf6735aa778733d043e
Martin Gaievski <gaievski@amazon.com>
2024-04-11 08:55:24 -0700

    Trying to fix bwc on linux by lowering threshold for jvm heap
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>

--- qa/rolling-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/AbstractRollingUpgradeTestCase.java
+++ qa/rolling-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/AbstractRollingUpgradeTestCase.java
@@ -130,4 +130,13 @@ public abstract class AbstractRollingUpgradeTestCase extends BaseNeuralSearchIT 
         );
         createPipelineProcessor(requestBody, pipelineName, modelId);
     }
+
+    @Override
+    protected void updateClusterSettings() {
+        updateClusterSettings("plugins.ml_commons.only_run_on_ml_node", false);
+        // default threshold for native circuit breaker is 90, it may be not enough on test runner machine
+        updateClusterSettings("plugins.ml_commons.native_memory_threshold", 100);
+        updateClusterSettings("plugins.ml_commons.jvm_heap_memory_threshold", 85);
+        updateClusterSettings("plugins.ml_commons.allow_registering_model_via_url", true);
+    }
 }
