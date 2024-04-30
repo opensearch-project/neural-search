3490a92b0c6318f25513c20398ef7c6710d0997a
yuye-aws <yuyezhu@amazon.com>
2024-04-30 20:14:58 +0800

    remove chunker util
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/processor/chunker/Chunker.java
+++ src/main/java/org/opensearch/neuralsearch/processor/chunker/Chunker.java
@@ -34,4 +34,16 @@ public interface Chunker {
      * @return chunked passages
      */
     List<String> chunk(String content, Map<String, Object> runtimeParameters);
+
+    /**
+     * Checks whether the chunking results would exceed the max chunk limit after adding a passage
+     * If exceeds, then return true
+     *
+     * @param chunkResultSize the size of chunking result
+     * @param runtimeMaxChunkLimit runtime max_chunk_limit, used to check with chunkResultSize
+     * @param stringTobeChunkedCount runtime string_tobe_chunked_count, used to check with chunkResultSize
+     */
+    static boolean checkRunTimeMaxChunkLimit(int chunkResultSize, int runtimeMaxChunkLimit, int stringTobeChunkedCount) {
+        return runtimeMaxChunkLimit != DISABLED_MAX_CHUNK_LIMIT && chunkResultSize + stringTobeChunkedCount >= runtimeMaxChunkLimit;
+    }
 }
