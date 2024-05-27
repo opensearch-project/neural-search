587fec7ee49036e9275030e8a79061b08478b379
Liyun Xiu <chishui2@gmail.com>
2024-05-27 15:08:42 +0800

    [FEATURE] Support batch ingestion in TextEmbeddingProcessor & SparseEncodingProcessor (#744)
    * Support batch ingestion in TextEmbeddingProcess & SparseEncodingProcessor
    
    Signed-off-by: Liyun Xiu <xiliyun@amazon.com>
    
    * Update Changelog
    
    Signed-off-by: Liyun Xiu <xiliyun@amazon.com>
    
    * Add UT and IT
    
    Signed-off-by: Liyun Xiu <xiliyun@amazon.com>
    
    * Add comments
    
    Signed-off-by: Liyun Xiu <xiliyun@amazon.com>
    
    * Sort texts by length before sending for inference
    
    Signed-off-by: Liyun Xiu <xiliyun@amazon.com>
    
    * Make consistent check for inferenceList
    
    Signed-off-by: Liyun Xiu <xiliyun@amazon.com>
    
    ---------
    
    Signed-off-by: Liyun Xiu <xiliyun@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/processor/SparseEncodingProcessor.java
+++ src/main/java/org/opensearch/neuralsearch/processor/SparseEncodingProcessor.java
@@ -7,6 +7,7 @@ package org.opensearch.neuralsearch.processor;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.env.Environment;
@@ -48,5 +49,14 @@ public final class SparseEncodingProcessor extends InferenceProcessor {
             setVectorFieldsToDocument(ingestDocument, ProcessMap, TokenWeightUtil.fetchListOfTokenWeightMap(resultMaps));
             handler.accept(ingestDocument, null);
         }, e -> { handler.accept(null, e); }));
+    }
+
+    @Override
+    public void doBatchExecute(List<String> inferenceList, Consumer<List<?>> handler, Consumer<Exception> onException) {
+        mlCommonsClientAccessor.inferenceSentencesWithMapResult(
+            this.modelId,
+            inferenceList,
+            ActionListener.wrap(resultMaps -> handler.accept(TokenWeightUtil.fetchListOfTokenWeightMap(resultMaps)), onException)
+        );
     }
 }
