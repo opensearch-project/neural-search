347e42e10ccb2122edf6f2283739739255dee09a
conggguan <congguan@amazon.com>
2024-06-11 10:45:47 +0800

    Remove redundant lines of two-phase BWC test.
    Signed-off-by: conggguan <congguan@amazon.com>

--- qa/restart-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/NeuralSparseTwoPhaseProcessorIT.java
+++ qa/restart-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/NeuralSparseTwoPhaseProcessorIT.java
@@ -16,28 +16,26 @@ import static org.opensearch.neuralsearch.util.TestUtils.TEXT_EMBEDDING_PROCESSO
 
 public class NeuralSparseTwoPhaseProcessorIT extends AbstractRestartUpgradeRestTestCase {
 
-    private static final String DENSE_INGEST_PIPELINE_NAME = "nstp-nlp-ingest-pipeline-dense";
+    private static final String NEURAL_SPARSE_INGEST_PIPELINE_NAME = "nstp-nlp-ingest-pipeline-dense";
     private static final String NEURAL_SPARSE_TWO_PHASE_SEARCH_PIPELINE_NAME = "nstp-nlp-two-phase-search-pipeline-sparse";
     private static final String TEST_ENCODING_FIELD = "passage_embedding";
     private static final String TEST_TEXT_FIELD = "passage_text";
     private static final String TEXT_1 = "Hello world a b";
 
-    public void testNeuralQueryTwoPhaseProcessor_NeuralSearch_E2EFlow() throws Exception {
+    public void testNeuralSparseQueryTwoPhaseProcessor_NeuralSearch_E2EFlow() throws Exception {
         waitForClusterHealthGreen(NODES_BWC_CLUSTER);
         NeuralSparseQueryBuilder neuralSparseQueryBuilder = new NeuralSparseQueryBuilder().fieldName(TEST_ENCODING_FIELD).queryText(TEXT_1);
         if (isRunningAgainstOldCluster()) {
             String modelId = uploadTextEmbeddingModel();
             loadModel(modelId);
             neuralSparseQueryBuilder.modelId(modelId);
-            createPipelineProcessor(modelId, DENSE_INGEST_PIPELINE_NAME);
+            createPipelineProcessor(modelId, NEURAL_SPARSE_INGEST_PIPELINE_NAME);
             createIndexWithConfiguration(
                 getIndexNameForTest(),
                 Files.readString(Path.of(classLoader.getResource("processor/IndexMappingMultipleShard.json").toURI())),
-                DENSE_INGEST_PIPELINE_NAME
+                NEURAL_SPARSE_INGEST_PIPELINE_NAME
             );
             addDocument(getIndexNameForTest(), "0", TEST_TEXT_FIELD, TEXT_1, null, null);
-
-            Object resultWithOut2PhasePipeline = search(getIndexNameForTest(), neuralSparseQueryBuilder, 1).get("hits");
             createNeuralSparseTwoPhaseSearchProcessor(NEURAL_SPARSE_TWO_PHASE_SEARCH_PIPELINE_NAME);
             updateIndexSettings(
                 getIndexNameForTest(),
@@ -45,22 +43,18 @@ public class NeuralSparseTwoPhaseProcessorIT extends AbstractRestartUpgradeRestT
             );
             Object resultWith2PhasePipeline = search(getIndexNameForTest(), neuralSparseQueryBuilder, 1).get("hits");
             assertNotNull(resultWith2PhasePipeline);
-            assertNotNull(resultWithOut2PhasePipeline);
         } else {
             String modelId = null;
             try {
-                modelId = TestUtils.getModelId(getIngestionPipeline(DENSE_INGEST_PIPELINE_NAME), TEXT_EMBEDDING_PROCESSOR);
+                modelId = TestUtils.getModelId(getIngestionPipeline(NEURAL_SPARSE_INGEST_PIPELINE_NAME), TEXT_EMBEDDING_PROCESSOR);
                 loadModel(modelId);
                 neuralSparseQueryBuilder.modelId(modelId);
                 Object resultWith2PhasePipeline = search(getIndexNameForTest(), neuralSparseQueryBuilder, 1).get("hits");
-                updateIndexSettings(getIndexNameForTest(), Settings.builder().put("index.search.default_pipeline", "_none"));
-                Object resultWithOut2PhasePipeline = search(getIndexNameForTest(), neuralSparseQueryBuilder, 1).get("hits");
                 assertNotNull(resultWith2PhasePipeline);
-                assertNotNull(resultWithOut2PhasePipeline);
             } finally {
                 wipeOfTestResources(
                     getIndexNameForTest(),
-                    DENSE_INGEST_PIPELINE_NAME,
+                    NEURAL_SPARSE_INGEST_PIPELINE_NAME,
                     modelId,
                     NEURAL_SPARSE_TWO_PHASE_SEARCH_PIPELINE_NAME
                 );
