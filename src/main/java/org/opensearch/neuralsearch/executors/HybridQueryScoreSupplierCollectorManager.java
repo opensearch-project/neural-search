244d15ae6de29e62fb0d3f462b3976a7a2a4af4b
Vijayan Balasubramanian <vijayan.balasubramanian@gmail.com>
2024-06-11 11:25:34 -0700

    Implement parallel execution of sub-queries for hybrid search (#781)
    * Implement parallel execution of sub-queries for hybrid search (#749)
    
    Add new thread pool to schedule tasks that are related to hybrid query execution
    Register executor builders with Plugin
    Use Lucene's Task Executor to execute and collect results
    Parallelize Query re-write
    Parallelize score supplier creation
    Parallelize build hybrid scores
    
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

---
+++ src/main/java/org/opensearch/neuralsearch/executors/HybridQueryScoreSupplierCollectorManager.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.opensearch.neuralsearch.executors;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.search.ScorerSupplier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * HybridQueryScoreSupplierCollectorManager is responsible for creating {@link HybridQueryExecutorCollector} instances.
+ * Useful to create {@link HybridQueryExecutorCollector} instances that build {@link ScorerSupplier} from
+ * given weight.
+ */
+@RequiredArgsConstructor
+public final class HybridQueryScoreSupplierCollectorManager
+    implements
+        HybridQueryExecutorCollectorManager<HybridQueryExecutorCollector<LeafReaderContext, ScorerSupplier>> {
+
+    private @NonNull LeafReaderContext context;
+
+    /**
+     * Creates new {@link HybridQueryExecutorCollector} instance everytime to facilitate parallel execution
+     * by individual tasks
+     * @return new instance of HybridQueryExecutorCollector
+     */
+    @Override
+    public HybridQueryExecutorCollector<LeafReaderContext, ScorerSupplier> newCollector() {
+        return HybridQueryExecutorCollector.newCollector(context);
+    }
+
+    /**
+     * mergeScoreSuppliers will build list of scoreSupplier from given list of collectors.
+     * This method should be called after HybridQueryExecutorCollector's collect method is called.
+     * If collectors didn't have any result, null will be added to list.
+     * This method must be called after collection is finished on all provided collectors.
+     * @param collectors List of collectors which is used to perform collection in parallel
+     * @return list of {@link ScorerSupplier}
+     */
+    public List<ScorerSupplier> mergeScoreSuppliers(List<HybridQueryExecutorCollector<LeafReaderContext, ScorerSupplier>> collectors) {
+        List<ScorerSupplier> scorerSuppliers = new ArrayList<>();
+        for (HybridQueryExecutorCollector<LeafReaderContext, ScorerSupplier> collector : collectors) {
+            Optional<ScorerSupplier> result = collector.getResult();
+            if (result.isPresent()) {
+                scorerSuppliers.add(result.get());
+            } else {
+                scorerSuppliers.add(null);
+            }
+        }
+        return scorerSuppliers;
+    }
+}
