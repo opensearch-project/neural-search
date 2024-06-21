f50c8b3e42bcc8c481c80edfc1611da82ec2ac90
Varun Jain <varunudr@amazon.com>
2024-06-21 11:56:14 -0700

    fix javadoc
    Signed-off-by: Varun Jain <varunudr@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/search/lucene/MultiLeafFieldComparator.java
+++ src/main/java/org/opensearch/neuralsearch/search/lucene/MultiLeafFieldComparator.java
@@ -37,7 +37,6 @@ public final class MultiLeafFieldComparator implements LeafFieldComparator {
 
     // Set the bottom slot, ie the "weakest" (sorted last) entry in the queue. When compareBottom is called, you should compare against this
     // slot. This will always be called before compareBottom.
-    // Params: slot – the currently weakest (sorted last) slot in the queue
     @Override
     public void setBottom(int slot) throws IOException {
         for (LeafFieldComparator comparator : comparators) {
@@ -48,9 +47,6 @@ public final class MultiLeafFieldComparator implements LeafFieldComparator {
     // Compare the bottom of the queue with this doc. This will only invoked after setBottom has been called. This should return the same
     // result as FieldComparator.compare(int, int)} as if bottom were slot1 and the new document were slot 2.
     // For a search that hits many results, this method will be the hotspot (invoked by far the most frequently).
-    // Params: doc – that was hit
-    // Returns: any N < 0 if the doc's value is sorted after the bottom entry (not competitive), any N > 0 if the doc's value is sorted
-    // before the bottom entry and 0 if they are equal.
     @Override
     public int compareBottom(int doc) throws IOException {
         // Compare the first comparator's result with reverse multiplier
@@ -71,9 +67,6 @@ public final class MultiLeafFieldComparator implements LeafFieldComparator {
     // Compare the top value with this doc. This will only invoked after setTopValue has been called.
     // This should return the same result as FieldComparator.compare(int, int)} as if topValue were slot1 and the new document were slot 2.
     // This is only called for searches that use searchAfter (deep paging).
-    // Params: doc – that was hit
-    // Returns: any N < 0 if the doc's value is sorted after the top entry (not competitive),any N > 0 if the doc's value is sorted before
-    // the top entry and 0 if they are equal.
     @Override
     public int compareTop(int doc) throws IOException {
         // Compare the first comparator's result with reverse multiplier
@@ -92,7 +85,6 @@ public final class MultiLeafFieldComparator implements LeafFieldComparator {
 
     // This method is called when a new hit is competitive.
     // You should copy any state associated with this document that will be required for future comparisons, into the specified slot.
-    // Params: slot – which slot to copy the hit to doc – docID relative to current reader
     @Override
     public void copy(int slot, int doc) throws IOException {
         for (LeafFieldComparator comparator : comparators) {
@@ -101,7 +93,6 @@ public final class MultiLeafFieldComparator implements LeafFieldComparator {
     }
 
     // Sets the Scorer to use in case a document's score is needed.
-    // Params: scorer – Scorer instance that you should use to obtain the current hit's score, if necessary.
     @Override
     public void setScorer(final Scorable scorer) throws IOException {
         for (LeafFieldComparator comparator : comparators) {
