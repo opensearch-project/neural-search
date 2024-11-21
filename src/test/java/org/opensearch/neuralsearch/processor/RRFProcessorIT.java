4a1a20db9c80bb6aaec15fae60f241c60b80b50e
Ryan Bogan <rbogan@amazon.com>
2024-11-21 12:23:54 -0800

    Address remaining feedback for integ test
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- src/test/java/org/opensearch/neuralsearch/processor/RRFProcessorIT.java
+++ src/test/java/org/opensearch/neuralsearch/processor/RRFProcessorIT.java
@@ -4,75 +4,101 @@
  */
 package org.opensearch.neuralsearch.processor;
 
-import com.google.common.collect.ImmutableList;
 import lombok.SneakyThrows;
-import org.apache.hc.core5.http.HttpHeaders;
-import org.apache.hc.core5.http.io.entity.EntityUtils;
-import org.apache.hc.core5.http.message.BasicHeader;
-import org.opensearch.client.Request;
-import org.opensearch.client.Response;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.xcontent.XContentFactory;
-import org.opensearch.common.xcontent.XContentHelper;
-import org.opensearch.common.xcontent.XContentType;
-import org.opensearch.core.rest.RestStatus;
-import org.opensearch.core.xcontent.XContentBuilder;
+import org.opensearch.index.query.MatchQueryBuilder;
 import org.opensearch.neuralsearch.BaseNeuralSearchIT;
+import org.opensearch.neuralsearch.query.HybridQueryBuilder;
+import org.opensearch.neuralsearch.query.NeuralQueryBuilder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.opensearch.neuralsearch.util.TestUtils.DEFAULT_USER_AGENT;
 import static org.opensearch.neuralsearch.util.TestUtils.DELTA_FOR_SCORE_ASSERTION;
 
 public class RRFProcessorIT extends BaseNeuralSearchIT {
 
     private int currentDoc = 1;
     private static final String RRF_INDEX_NAME = "rrf-index";
-    private static final String RRF_SEARCH_PIPELINE = "rrf-search-pipeline";
 
     @SneakyThrows
     public void testRRF_whenValidInput_thenSucceed() {
-        String modelId = prepareModel();
         String ingestPipelineName = "rrf-ingest-pipeline";
-        createPipelineProcessor(modelId, ingestPipelineName, ProcessorType.TEXT_EMBEDDING);
-        Settings indexSettings = Settings.builder().put("index.knn", true).put("default_pipeline", ingestPipelineName).build();
-        String indexMappings = XContentFactory.jsonBuilder()
-            .startObject()
-            .startObject("properties")
-            .startObject("id")
-            .field("type", "text")
-            .endObject()
-            .startObject("passage_embedding")
-            .field("type", "knn_vector")
-            .field("dimension", "768")
-            .startObject("method")
-            .field("engine", "lucene")
-            .field("space_type", "l2")
-            .field("name", "hnsw")
-            .endObject()
-            .endObject()
-            .startObject("text")
-            .field("type", "text")
-            .endObject()
-            .endObject()
-            .endObject()
-            .toString();
-        // Removes the {} around the string, since they are already included with createIndex
-        indexMappings = indexMappings.substring(1, indexMappings.length() - 1);
-        String indexName = "rrf-index";
-        createIndex(indexName, indexSettings, indexMappings, null);
-        addDocuments();
-        createDefaultRRFSearchPipeline();
+        String modelId = null;
+        try {
+            modelId = prepareModel();
+            createPipelineProcessor(modelId, ingestPipelineName, ProcessorType.TEXT_EMBEDDING);
+            Settings indexSettings = Settings.builder().put("index.knn", true).put("default_pipeline", ingestPipelineName).build();
+            String indexMappings = XContentFactory.jsonBuilder()
+                .startObject()
+                .startObject("properties")
+                .startObject("id")
+                .field("type", "text")
+                .endObject()
+                .startObject("passage_embedding")
+                .field("type", "knn_vector")
+                .field("dimension", "768")
+                .startObject("method")
+                .field("engine", "lucene")
+                .field("space_type", "l2")
+                .field("name", "hnsw")
+                .endObject()
+                .endObject()
+                .startObject("text")
+                .field("type", "text")
+                .endObject()
+                .endObject()
+                .endObject()
+                .toString();
+            // Removes the {} around the string, since they are already included with createIndex
+            indexMappings = indexMappings.substring(1, indexMappings.length() - 1);
+            createIndex(RRF_INDEX_NAME, indexSettings, indexMappings, null);
+            addDocuments();
+            createDefaultRRFSearchPipeline();
 
-        Map<String, Object> results = searchRRF(modelId);
-        Map<String, Object> hits = (Map<String, Object>) results.get("hits");
-        ArrayList<HashMap<String, Object>> hitsList = (ArrayList<HashMap<String, Object>>) hits.get("hits");
-        assertEquals(3, hitsList.size());
-        assertEquals(0.016393442, (Double) hitsList.getFirst().get("_score"), DELTA_FOR_SCORE_ASSERTION);
-        assertEquals(0.016129032, (Double) hitsList.get(1).get("_score"), DELTA_FOR_SCORE_ASSERTION);
-        assertEquals(0.015873017, (Double) hitsList.getLast().get("_score"), DELTA_FOR_SCORE_ASSERTION);
+            HybridQueryBuilder hybridQueryBuilder = getHybridQueryBuilder(modelId);
+
+            Map<String, Object> results = search(
+                RRF_INDEX_NAME,
+                hybridQueryBuilder,
+                null,
+                5,
+                Map.of("search_pipeline", RRF_SEARCH_PIPELINE)
+            );
+            Map<String, Object> hits = (Map<String, Object>) results.get("hits");
+            ArrayList<HashMap<String, Object>> hitsList = (ArrayList<HashMap<String, Object>>) hits.get("hits");
+            assertEquals(3, hitsList.size());
+            assertEquals(0.016393442, (Double) hitsList.getFirst().get("_score"), DELTA_FOR_SCORE_ASSERTION);
+            assertEquals(0.016129032, (Double) hitsList.get(1).get("_score"), DELTA_FOR_SCORE_ASSERTION);
+            assertEquals(0.015873017, (Double) hitsList.getLast().get("_score"), DELTA_FOR_SCORE_ASSERTION);
+        } finally {
+            wipeOfTestResources(RRF_INDEX_NAME, ingestPipelineName, modelId, RRF_SEARCH_PIPELINE);
+        }
+    }
+
+    private HybridQueryBuilder getHybridQueryBuilder(String modelId) {
+        MatchQueryBuilder matchQueryBuilder = new MatchQueryBuilder("text", "cowboy rodeo bronco");
+
+        NeuralQueryBuilder neuralQueryBuilder = new NeuralQueryBuilder(
+            "passage_embedding",
+            "wild_west",
+            "",
+            modelId,
+            5,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        );
+
+        HybridQueryBuilder hybridQueryBuilder = new HybridQueryBuilder();
+        hybridQueryBuilder.add(matchQueryBuilder);
+        hybridQueryBuilder.add(neuralQueryBuilder);
+        return hybridQueryBuilder;
     }
 
     @SneakyThrows
@@ -93,74 +119,5 @@ public class RRFProcessorIT extends BaseNeuralSearchIT {
     @SneakyThrows
     private void addDocument(String description, String imageText) {
         addDocument(RRF_INDEX_NAME, String.valueOf(currentDoc++), "text", description, "image_text", imageText);
-    }
-
-    @SneakyThrows
-    private void createDefaultRRFSearchPipeline() {
-        String requestBody = XContentFactory.jsonBuilder()
-            .startObject()
-            .field("description", "Post processor for hybrid search")
-            .startArray("phase_results_processors")
-            .startObject()
-            .startObject("score-ranker-processor")
-            .startObject("combination")
-            .field("technique", "rrf")
-            .endObject()
-            .endObject()
-            .endObject()
-            .endArray()
-            .endObject()
-            .toString();
-
-        makeRequest(
-            client(),
-            "PUT",
-            String.format(LOCALE, "/_search/pipeline/%s", RRF_SEARCH_PIPELINE),
-            null,
-            toHttpEntity(String.format(LOCALE, requestBody)),
-            ImmutableList.of(new BasicHeader(HttpHeaders.USER_AGENT, DEFAULT_USER_AGENT))
-        );
-    }
-
-    @SneakyThrows
-    private Map<String, Object> searchRRF(String modelId) {
-        XContentBuilder builder = XContentFactory.jsonBuilder()
-            .startObject()
-            .startObject("_source")
-            .startArray("exclude")
-            .value("passage_embedding")
-            .endArray()
-            .endObject()
-            .startObject("query")
-            .startObject("hybrid")
-            .startArray("queries")
-            .startObject()
-            .startObject("match")
-            .startObject("text")
-            .field("query", "cowboy rodeo bronco")
-            .endObject()
-            .endObject()
-            .endObject()
-            .startObject()
-            .startObject("neural")
-            .startObject("passage_embedding")
-            .field("query_text", "wild west")
-            .field("model_id", modelId)
-            .field("k", 5)
-            .endObject()
-            .endObject()
-            .endObject()
-            .endArray()
-            .endObject()
-            .endObject()
-            .endObject();
-
-        Request request = new Request("GET", "/" + RRF_INDEX_NAME + "/_search?timeout=1000s&search_pipeline=" + RRF_SEARCH_PIPELINE);
-        request.setJsonEntity(builder.toString());
-        Response response = client().performRequest(request);
-        assertEquals(request.getEndpoint() + ": failed", RestStatus.OK, RestStatus.fromCode(response.getStatusLine().getStatusCode()));
-
-        String responseBody = EntityUtils.toString(response.getEntity());
-        return XContentHelper.convertToMap(XContentType.JSON.xContent(), responseBody, false);
     }
 }
