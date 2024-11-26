f539741bda29f6c204d42566d107ae06337383f4
Ryan Bogan <rbogan@amazon.com>
2024-11-26 09:28:42 -0800

    Address integration test feedback
    Signed-off-by: Ryan Bogan <rbogan@amazon.com>

--- src/test/java/org/opensearch/neuralsearch/processor/RRFProcessorIT.java
+++ src/test/java/org/opensearch/neuralsearch/processor/RRFProcessorIT.java
@@ -5,60 +5,40 @@
 package org.opensearch.neuralsearch.processor;
 
 import lombok.SneakyThrows;
-import org.opensearch.common.settings.Settings;
-import org.opensearch.common.xcontent.XContentFactory;
 import org.opensearch.index.query.MatchQueryBuilder;
+import org.opensearch.knn.index.query.KNNQueryBuilder;
 import org.opensearch.neuralsearch.BaseNeuralSearchIT;
 import org.opensearch.neuralsearch.query.HybridQueryBuilder;
-import org.opensearch.neuralsearch.query.NeuralQueryBuilder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.opensearch.neuralsearch.util.TestUtils.DELTA_FOR_SCORE_ASSERTION;
+import static org.opensearch.neuralsearch.util.TestUtils.TEST_SPACE_TYPE;
 
 public class RRFProcessorIT extends BaseNeuralSearchIT {
 
     private int currentDoc = 1;
     private static final String RRF_INDEX_NAME = "rrf-index";
+    private static final String RRF_SEARCH_PIPELINE = "rrf-search-pipeline";
+    private static final String RRF_INGEST_PIPELINE = "rrf-ingest-pipeline";
+
+    private static final int RRF_DIMENSION = 5;
 
     @SneakyThrows
     public void testRRF_whenValidInput_thenSucceed() {
-        String ingestPipelineName = "rrf-ingest-pipeline";
-        String modelId = null;
         try {
-            modelId = prepareModel();
-            createPipelineProcessor(modelId, ingestPipelineName, ProcessorType.TEXT_EMBEDDING);
-            Settings indexSettings = Settings.builder().put("index.knn", true).put("default_pipeline", ingestPipelineName).build();
-            String indexMappings = XContentFactory.jsonBuilder()
-                .startObject()
-                .startObject("properties")
-                .startObject("id")
-                .field("type", "text")
-                .endObject()
-                .startObject("passage_embedding")
-                .field("type", "knn_vector")
-                .field("dimension", "768")
-                .startObject("method")
-                .field("engine", "lucene")
-                .field("space_type", "l2")
-                .field("name", "hnsw")
-                .endObject()
-                .endObject()
-                .startObject("text")
-                .field("type", "text")
-                .endObject()
-                .endObject()
-                .endObject()
-                .toString();
-            // Removes the {} around the string, since they are already included with createIndex
-            indexMappings = indexMappings.substring(1, indexMappings.length() - 1);
-            createIndex(RRF_INDEX_NAME, indexSettings, indexMappings, null);
+            createPipelineProcessor(null, RRF_INGEST_PIPELINE, ProcessorType.TEXT_EMBEDDING);
+            prepareKnnIndex(
+                RRF_INDEX_NAME,
+                Collections.singletonList(new KNNFieldConfig("passage_embedding", RRF_DIMENSION, TEST_SPACE_TYPE))
+            );
             addDocuments();
             createDefaultRRFSearchPipeline();
 
-            HybridQueryBuilder hybridQueryBuilder = getHybridQueryBuilder(modelId);
+            HybridQueryBuilder hybridQueryBuilder = getHybridQueryBuilder();
 
             Map<String, Object> results = search(
                 RRF_INDEX_NAME,
@@ -74,30 +54,20 @@ public class RRFProcessorIT extends BaseNeuralSearchIT {
             assertEquals(0.016129032, (Double) hitsList.get(1).get("_score"), DELTA_FOR_SCORE_ASSERTION);
             assertEquals(0.015873017, (Double) hitsList.getLast().get("_score"), DELTA_FOR_SCORE_ASSERTION);
         } finally {
-            wipeOfTestResources(RRF_INDEX_NAME, ingestPipelineName, modelId, RRF_SEARCH_PIPELINE);
+            wipeOfTestResources(RRF_INDEX_NAME, RRF_INGEST_PIPELINE, null, RRF_SEARCH_PIPELINE);
         }
     }
 
-    private HybridQueryBuilder getHybridQueryBuilder(String modelId) {
+    private HybridQueryBuilder getHybridQueryBuilder() {
         MatchQueryBuilder matchQueryBuilder = new MatchQueryBuilder("text", "cowboy rodeo bronco");
-
-        NeuralQueryBuilder neuralQueryBuilder = new NeuralQueryBuilder(
-            "passage_embedding",
-            "wild_west",
-            "",
-            modelId,
-            5,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null
-        );
+        KNNQueryBuilder knnQueryBuilder = new KNNQueryBuilder.Builder().fieldName("passage_embedding")
+            .k(5)
+            .vector(new float[] { 0.1f, 1.2f, 2.3f, 3.4f, 4.5f })
+            .build();
 
         HybridQueryBuilder hybridQueryBuilder = new HybridQueryBuilder();
         hybridQueryBuilder.add(matchQueryBuilder);
-        hybridQueryBuilder.add(neuralQueryBuilder);
+        hybridQueryBuilder.add(knnQueryBuilder);
         return hybridQueryBuilder;
     }
 
