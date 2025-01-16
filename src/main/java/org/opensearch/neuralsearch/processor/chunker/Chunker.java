d58d9d389fefd12c5c56da010f86c96aaf616486
yuye-aws <yuyezhu@amazon.com>
2025-01-16 09:05:28 +0800

    Enhance: Add comments to chunking-related code (#1106)
    * add logs for chunker parameter parser
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * add logs for delimiter chunker
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * add comments for delimiter chunker
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * update comments for delimiter chunker
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * update comments for chunker interface
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * add comments for chunker facdtory
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    * update comments for chunker interface
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    
    ---------
    
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>
    (cherry picked from commit 3130de36cb2ec6d08faa6f5301cee5ad69d359bd)

--- src/main/java/org/opensearch/neuralsearch/processor/chunker/Chunker.java
+++ src/main/java/org/opensearch/neuralsearch/processor/chunker/Chunker.java
@@ -13,9 +13,16 @@ import java.util.List;
  */
 public interface Chunker {
 
+    /** Field name for specifying the maximum chunk limit in the configuration. */
     String MAX_CHUNK_LIMIT_FIELD = "max_chunk_limit";
+
+    /** Field name for tracking the count of chunked strings. */
     String CHUNK_STRING_COUNT_FIELD = "chunk_string_count";
+
+    /** Default maximum number of chunks allowed (100). */
     int DEFAULT_MAX_CHUNK_LIMIT = 100;
+
+    /** Special value (-1) indicating that chunk limiting is disabled. */
     int DISABLED_MAX_CHUNK_LIMIT = -1;
 
     /**
@@ -42,6 +49,7 @@ public interface Chunker {
      * @param chunkResultSize the size of chunking result
      * @param runtimeMaxChunkLimit runtime max_chunk_limit, used to check with chunkResultSize
      * @param chunkStringCount runtime chunk_string_count, used to check with chunkResultSize
+     * @return true if adding the new chunks would exceed the limit, false otherwise
      */
     static boolean checkRunTimeMaxChunkLimit(int chunkResultSize, int runtimeMaxChunkLimit, int chunkStringCount) {
         return runtimeMaxChunkLimit != DISABLED_MAX_CHUNK_LIMIT && chunkResultSize + chunkStringCount >= runtimeMaxChunkLimit;
