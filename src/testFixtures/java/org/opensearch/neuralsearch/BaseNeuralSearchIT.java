d17a88f5b726eac72854d5a4008cd0615926a2cb
will-hwang <sang7239@gmail.com>
2025-02-04 22:18:14 -0800

    make pipeline nullable
    Signed-off-by: will-hwang <sang7239@gmail.com>

--- src/testFixtures/java/org/opensearch/neuralsearch/BaseNeuralSearchIT.java
+++ src/testFixtures/java/org/opensearch/neuralsearch/BaseNeuralSearchIT.java
@@ -1423,7 +1423,9 @@ public abstract class BaseNeuralSearchIT extends OpenSearchSecureRestTestCase {
     protected Map<String, Object> getIngestionPipeline(final String pipelineName) {
         Request request = new Request("GET", "/_ingest/pipeline/" + pipelineName);
         Response response = client().performRequest(request);
-        assertEquals(request.getEndpoint() + ": failed", RestStatus.OK, RestStatus.fromCode(response.getStatusLine().getStatusCode()));
+        if (RestStatus.fromCode(response.getStatusLine().getStatusCode()).equals(RestStatus.NOT_FOUND)) {
+            return null;
+        }
         String responseBody = EntityUtils.toString(response.getEntity());
         Map<String, Object> responseMap = createParser(XContentType.JSON.xContent(), responseBody).map();
         return (Map<String, Object>) responseMap.get(pipelineName);
