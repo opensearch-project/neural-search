bf58371793bac38d51581c126d82114b1e5ec0d3
will-hwang <sang7239@gmail.com>
2025-02-06 09:45:40 -0800

    add more debug line
    Signed-off-by: will-hwang <sang7239@gmail.com>

--- qa/restart-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/restart/SemanticSearchIT.java
+++ qa/restart-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/restart/SemanticSearchIT.java
@@ -35,7 +35,7 @@ public class SemanticSearchIT extends AbstractRestartUpgradeRestTestCase {
             addDocument(getIndexNameForTest(), "0", TEST_FIELD, TEXT, null, null);
         } else {
             String modelId = null;
-            fail("Text Embedding model state [" + TextEmbeddingModel.getInstance() + "]");
+            fail("Text Embedding model state [" + TextEmbeddingModel.getInstance().toString() + "]");
             try {
                 modelId = TextEmbeddingModel.getInstance().getModelId();
                 loadModel(modelId);
