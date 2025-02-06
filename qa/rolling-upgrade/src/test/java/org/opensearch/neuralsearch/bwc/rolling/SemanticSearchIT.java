a75011ff362c15b3c706f8c07fe848dd4073bc75
will-hwang <sang7239@gmail.com>
2025-02-05 22:33:52 -0800

    Revert "initializing model before tests"
    This reverts commit 0ec949cded106d62c2d4473aae9f2214db8e52a3.
    
    Signed-off-by: will-hwang <sang7239@gmail.com>

--- qa/rolling-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/rolling/SemanticSearchIT.java
+++ qa/rolling-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/rolling/SemanticSearchIT.java
@@ -8,6 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 import static org.opensearch.neuralsearch.util.TestUtils.NODES_BWC_CLUSTER;
+import static org.opensearch.neuralsearch.util.TestUtils.TEXT_EMBEDDING_PROCESSOR;
+import static org.opensearch.neuralsearch.util.TestUtils.getModelId;
 import org.opensearch.neuralsearch.query.NeuralQueryBuilder;
 
 public class SemanticSearchIT extends AbstractRollingUpgradeTestCase {
@@ -17,6 +19,7 @@ public class SemanticSearchIT extends AbstractRollingUpgradeTestCase {
     private static final String TEXT_MIXED = "Hello world mixed";
     private static final String TEXT_UPGRADED = "Hello world upgraded";
     private static final int NUM_DOCS_PER_ROUND = 1;
+    private static String modelId = "";
 
     // Test rolling-upgrade Semantic Search
     // Create Text Embedding Processor, Ingestion Pipeline and add document
@@ -25,8 +28,9 @@ public class SemanticSearchIT extends AbstractRollingUpgradeTestCase {
         waitForClusterHealthGreen(NODES_BWC_CLUSTER, 90);
         switch (getClusterType()) {
             case OLD:
-                loadModel(textEmbeddingModelId);
-                createPipelineProcessor(textEmbeddingModelId, PIPELINE_NAME);
+                modelId = uploadTextEmbeddingModel();
+                loadModel(modelId);
+                createPipelineProcessor(modelId, PIPELINE_NAME);
                 createIndexWithConfiguration(
                     getIndexNameForTest(),
                     Files.readString(Path.of(classLoader.getResource("processor/IndexMappings.json").toURI())),
@@ -35,24 +39,26 @@ public class SemanticSearchIT extends AbstractRollingUpgradeTestCase {
                 addDocument(getIndexNameForTest(), "0", TEST_FIELD, TEXT, null, null);
                 break;
             case MIXED:
+                modelId = getModelId(getIngestionPipeline(PIPELINE_NAME), TEXT_EMBEDDING_PROCESSOR);
                 int totalDocsCountMixed;
                 if (isFirstMixedRound()) {
                     totalDocsCountMixed = NUM_DOCS_PER_ROUND;
-                    validateTestIndexOnUpgrade(totalDocsCountMixed, textEmbeddingModelId, TEXT);
+                    validateTestIndexOnUpgrade(totalDocsCountMixed, modelId, TEXT);
                     addDocument(getIndexNameForTest(), "1", TEST_FIELD, TEXT_MIXED, null, null);
                 } else {
                     totalDocsCountMixed = 2 * NUM_DOCS_PER_ROUND;
-                    validateTestIndexOnUpgrade(totalDocsCountMixed, textEmbeddingModelId, TEXT_MIXED);
+                    validateTestIndexOnUpgrade(totalDocsCountMixed, modelId, TEXT_MIXED);
                 }
                 break;
             case UPGRADED:
                 try {
+                    modelId = getModelId(getIngestionPipeline(PIPELINE_NAME), TEXT_EMBEDDING_PROCESSOR);
                     int totalDocsCountUpgraded = 3 * NUM_DOCS_PER_ROUND;
-                    loadModel(textImageEmbeddingModelId);
+                    loadModel(modelId);
                     addDocument(getIndexNameForTest(), "2", TEST_FIELD, TEXT_UPGRADED, null, null);
-                    validateTestIndexOnUpgrade(totalDocsCountUpgraded, textImageEmbeddingModelId, TEXT_UPGRADED);
+                    validateTestIndexOnUpgrade(totalDocsCountUpgraded, modelId, TEXT_UPGRADED);
                 } finally {
-                    wipeOfTestResources(getIndexNameForTest(), PIPELINE_NAME, null, null);
+                    wipeOfTestResources(getIndexNameForTest(), PIPELINE_NAME, modelId, null);
                 }
                 break;
             default:
