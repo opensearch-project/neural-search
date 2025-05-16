2fc1e14428990cf3239121842bb64f04f6d64924
Andy Qin <qinandy@amazon.com>
2025-05-16 16:23:17 -0700

    Add normalization processor and RRF processor stats

--- src/test/java/org/opensearch/neuralsearch/processor/RRFProcessorIT.java
+++ src/test/java/org/opensearch/neuralsearch/processor/RRFProcessorIT.java
@@ -9,6 +9,8 @@ import org.opensearch.index.query.MatchQueryBuilder;
 import org.opensearch.knn.index.query.KNNQueryBuilder;
 import org.opensearch.neuralsearch.BaseNeuralSearchIT;
 import org.opensearch.neuralsearch.query.HybridQueryBuilder;
+import org.opensearch.neuralsearch.stats.events.EventStatName;
+import org.opensearch.neuralsearch.stats.info.InfoStatName;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -43,6 +45,32 @@ public class RRFProcessorIT extends BaseNeuralSearchIT {
         assertEquals(0.016393442, (Double) hitsList.getFirst().get("_score"), DELTA_FOR_SCORE_ASSERTION);
         assertEquals(0.016129032, (Double) hitsList.get(1).get("_score"), DELTA_FOR_SCORE_ASSERTION);
         assertEquals(0.015873017, (Double) hitsList.getLast().get("_score"), DELTA_FOR_SCORE_ASSERTION);
+    }
+
+    @SneakyThrows
+    public void testRRF_statsEnabled() {
+        updateClusterSettings("plugins.neural_search.stats_enabled", true);
+
+        createPipelineProcessor(null, RRF_INGEST_PIPELINE, ProcessorType.TEXT_EMBEDDING);
+        prepareKnnIndex(RRF_INDEX_NAME, Collections.singletonList(new KNNFieldConfig("passage_embedding", RRF_DIMENSION, TEST_SPACE_TYPE)));
+        addDocuments();
+        createDefaultRRFSearchPipeline();
+
+        HybridQueryBuilder hybridQueryBuilder = getHybridQueryBuilder();
+
+        Map<String, Object> results = search(RRF_INDEX_NAME, hybridQueryBuilder, null, 5, Map.of("search_pipeline", RRF_SEARCH_PIPELINE));
+
+        // Get stats
+        String responseBody = executeNeuralStatRequest(new ArrayList<>(), new ArrayList<>());
+        Map<String, Object> stats = parseInfoStatsResponse(responseBody);
+        Map<String, Object> allNodesStats = parseAggregatedNodeStatsResponse(responseBody);
+
+        // Parse json to get stats
+        assertEquals(1, getNestedValue(allNodesStats, EventStatName.RRF_PROCESSOR_EXECUTIONS));
+
+        assertEquals(1, getNestedValue(stats, InfoStatName.RRF_PROCESSORS));
+
+        updateClusterSettings("plugins.neural_search.stats_enabled", false);
     }
 
     private HybridQueryBuilder getHybridQueryBuilder() {
