4507ce4408b67410c58ad02579b306e8ea2f9e15
Liyun Xiu <xiliyun@amazon.com>
2025-06-26 12:36:36 +0800

    Fix a code merge issue (#65)
    Signed-off-by: Liyun Xiu <xiliyun@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/sparse/common/PredicateUtils.java
+++ src/main/java/org/opensearch/neuralsearch/sparse/common/PredicateUtils.java
@@ -6,16 +6,17 @@ package org.opensearch.neuralsearch.sparse.common;
 
 import org.apache.lucene.index.FieldInfo;
 import org.apache.lucene.index.SegmentInfo;
-import org.opensearch.neuralsearch.sparse.mapper.SparseMethodContext;
 
 import java.util.function.BiPredicate;
+
+import static org.opensearch.neuralsearch.sparse.common.SparseConstants.ALGO_TRIGGER_DOC_COUNT_FIELD;
 
 public class PredicateUtils {
 
     public static final BiPredicate<SegmentInfo, FieldInfo> shouldRunSeisPredicate = new BiPredicate<>() {
         @Override
         public boolean test(SegmentInfo segmentInfo, FieldInfo fieldInfo) {
-            int clusterUntilDocCountReach = Integer.parseInt(fieldInfo.attributes().get(SparseMethodContext.CLUSTER_UNTIL_FIELD));
+            int clusterUntilDocCountReach = Integer.parseInt(fieldInfo.attributes().get(ALGO_TRIGGER_DOC_COUNT_FIELD));
             return segmentInfo.maxDoc() >= clusterUntilDocCountReach;
         }
     };
