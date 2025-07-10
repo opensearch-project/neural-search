269822061e20ed7efc02e195dd5834f8db977e82
Bo Zhang <bzhangam@amazon.com>
2025-07-10 13:59:58 -0700

    Support configuring the ingest batch size for the semantic field.
    Signed-off-by: Bo Zhang <bzhangam@amazon.com>

--- src/test/java/org/opensearch/neuralsearch/processor/factory/SemanticFieldProcessorFactoryTests.java
+++ src/test/java/org/opensearch/neuralsearch/processor/factory/SemanticFieldProcessorFactoryTests.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.settings.Settings;
 import org.opensearch.env.Environment;
 import org.opensearch.index.analysis.AnalysisRegistry;
 import org.opensearch.neuralsearch.mapper.SemanticFieldMapper;
@@ -23,8 +24,11 @@ import static org.opensearch.neuralsearch.constants.MappingConstants.DOC;
 import static org.opensearch.neuralsearch.constants.MappingConstants.PROPERTIES;
 import static org.opensearch.neuralsearch.constants.MappingConstants.TYPE;
 import static org.opensearch.neuralsearch.processor.TextChunkingProcessorTests.getAnalysisRegistry;
+import static org.opensearch.neuralsearch.settings.NeuralSearchSettings.SEMANTIC_INGEST_BATCH_SIZE;
 import static org.opensearch.plugins.IngestPlugin.SystemIngestPipelineConfigKeys.INDEX_MAPPINGS;
+import static org.opensearch.plugins.IngestPlugin.SystemIngestPipelineConfigKeys.INDEX_SETTINGS;
 import static org.opensearch.plugins.IngestPlugin.SystemIngestPipelineConfigKeys.INDEX_TEMPLATE_MAPPINGS;
+import static org.opensearch.plugins.IngestPlugin.SystemIngestPipelineConfigKeys.INDEX_TEMPLATE_SETTINGS;
 
 public class SemanticFieldProcessorFactoryTests extends OpenSearchTestCase {
     @Mock
@@ -50,32 +54,48 @@ public class SemanticFieldProcessorFactoryTests extends OpenSearchTestCase {
     }
 
     public void testNewProcessor_indexMappingsWithoutProperties_thenReturnNull() {
-        Map<String, Object> mappings = Map.of(INDEX_MAPPINGS, Map.of(DOC, Map.of()));
-        final SemanticFieldProcessor processor = (SemanticFieldProcessor) factory.newProcessor(null, null, mappings);
+        Map<String, Object> processorConfig = Map.of(INDEX_MAPPINGS, Map.of(DOC, Map.of()));
+        final SemanticFieldProcessor processor = (SemanticFieldProcessor) factory.newProcessor(null, null, processorConfig);
         assertNull(processor);
     }
 
     public void testNewProcessor_templateMappingsWithoutProperties_thenReturnNull() {
-        Map<String, Object> mappings = Map.of(INDEX_TEMPLATE_MAPPINGS, List.of(Map.of(DOC, Map.of())));
-        final SemanticFieldProcessor processor = (SemanticFieldProcessor) factory.newProcessor(null, null, mappings);
+        Map<String, Object> processorConfig = Map.of(INDEX_TEMPLATE_MAPPINGS, List.of(Map.of(DOC, Map.of())));
+        final SemanticFieldProcessor processor = (SemanticFieldProcessor) factory.newProcessor(null, null, processorConfig);
         assertNull(processor);
     }
 
     public void testNewProcessor_indexMappingsWithSemanticField_thenCreateProcessor() {
-        Map<String, Object> mappings = Map.of(
+        Map<String, Object> processorConfig = Map.of(
             INDEX_MAPPINGS,
             Map.of(DOC, Map.of(PROPERTIES, Map.of("semantic_field", Map.of(TYPE, SemanticFieldMapper.CONTENT_TYPE))))
         );
-        final SemanticFieldProcessor processor = (SemanticFieldProcessor) factory.newProcessor(null, null, mappings);
+        final SemanticFieldProcessor processor = (SemanticFieldProcessor) factory.newProcessor(null, null, processorConfig);
         assertNotNull(processor);
+        assertEquals(10, processor.getBatchSize());
     }
 
-    public void testNewProcessor_templateIndexMappingsWithSemanticField_thenCreateProcessor() {
-        Map<String, Object> mappings = Map.of(
-            INDEX_TEMPLATE_MAPPINGS,
-            List.of(Map.of(DOC, Map.of(PROPERTIES, Map.of("semantic_field", Map.of(TYPE, SemanticFieldMapper.CONTENT_TYPE)))))
+    public void testNewProcessor_indexMappingsWithSemanticFieldAndCustomBatchSize_thenCreateProcessor() {
+        Map<String, Object> processorConfig = Map.of(
+            INDEX_MAPPINGS,
+            Map.of(DOC, Map.of(PROPERTIES, Map.of("semantic_field", Map.of(TYPE, SemanticFieldMapper.CONTENT_TYPE)))),
+            INDEX_SETTINGS,
+            Settings.builder().put(SEMANTIC_INGEST_BATCH_SIZE.getKey(), 1).build()
         );
-        final SemanticFieldProcessor processor = (SemanticFieldProcessor) factory.newProcessor(null, null, mappings);
+        final SemanticFieldProcessor processor = (SemanticFieldProcessor) factory.newProcessor(null, null, processorConfig);
         assertNotNull(processor);
+        assertEquals(1, processor.getBatchSize());
+    }
+
+    public void testNewProcessor_templateIndexMappingsWithSemanticFieldAndCustomBatchSize_thenCreateProcessor() {
+        Map<String, Object> processorConfig = Map.of(
+            INDEX_TEMPLATE_MAPPINGS,
+            List.of(Map.of(DOC, Map.of(PROPERTIES, Map.of("semantic_field", Map.of(TYPE, SemanticFieldMapper.CONTENT_TYPE))))),
+            INDEX_TEMPLATE_SETTINGS,
+            List.of(Settings.builder().build(), Settings.builder().put(SEMANTIC_INGEST_BATCH_SIZE.getKey(), 100).build())
+        );
+        final SemanticFieldProcessor processor = (SemanticFieldProcessor) factory.newProcessor(null, null, processorConfig);
+        assertNotNull(processor);
+        assertEquals(100, processor.getBatchSize());
     }
 }
