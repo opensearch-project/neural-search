de00a442bcb510db7032e071a756d991e19914c8
Owais <owaiskazi19@gmail.com>
2025-07-28 13:38:39 -0700

    Added multiple shards integ tests
    Signed-off-by: Owais <owaiskazi19@gmail.com>

--- src/test/java/org/opensearch/neuralsearch/query/AgenticSearchQueryIT.java
+++ src/test/java/org/opensearch/neuralsearch/query/AgenticSearchQueryIT.java
@@ -8,7 +8,9 @@ import org.opensearch.neuralsearch.BaseNeuralSearchIT;
 import org.opensearch.client.Request;
 import org.opensearch.client.Response;
 
+import java.util.Collections;
 import java.util.Locale;
+import java.util.Map;
 
 public class AgenticSearchQueryIT extends BaseNeuralSearchIT {
 
@@ -17,8 +19,7 @@ public class AgenticSearchQueryIT extends BaseNeuralSearchIT {
     private static final String TEST_QUERY_TEXT = "Find documents about machine learning";
 
     public void testAgenticSearchQuery_withValidParameters_thenSuccess() throws Exception {
-        createIndex(TEST_INDEX, "{\"mappings\":{\"properties\":{\"passage_text\":{\"type\":\"text\"}}}}");
-        addDocument(TEST_INDEX, "1", "passage_text", "This is about science and technology", null, null);
+        initializeIndexIfNotExist(TEST_INDEX);
 
         String queryJson = String.format(
             Locale.ROOT,
@@ -36,7 +37,7 @@ public class AgenticSearchQueryIT extends BaseNeuralSearchIT {
     }
 
     public void testAgenticSearchQuery_withMissingQueryText_thenFail() throws Exception {
-        createIndex(TEST_INDEX, "{\"mappings\":{\"properties\":{\"text\":{\"type\":\"text\"}}}}");
+        initializeIndexIfNotExist(TEST_INDEX);
 
         String queryJson = String.format(
             Locale.ROOT,
@@ -59,7 +60,7 @@ public class AgenticSearchQueryIT extends BaseNeuralSearchIT {
     }
 
     public void testAgenticSearchQuery_withMissingAgentId_thenFail() throws Exception {
-        createIndex(TEST_INDEX, "{\"mappings\":{\"properties\":{\"text\":{\"type\":\"text\"}}}}");
+        initializeIndexIfNotExist(TEST_INDEX);
 
         String queryJson = String.format(
             Locale.ROOT,
@@ -82,8 +83,7 @@ public class AgenticSearchQueryIT extends BaseNeuralSearchIT {
     }
 
     public void testAgenticSearchQuery_withQueryFields_thenSuccess() throws Exception {
-        createIndex(TEST_INDEX, "{\"mappings\":{\"properties\":{\"passage_text\":{\"type\":\"text\"}}}}");
-        addDocument(TEST_INDEX, "1", "passage_text", "This is about science and technology", null, null);
+        initializeIndexIfNotExist(TEST_INDEX);
 
         String queryJson = String.format(
             Locale.ROOT,
@@ -106,5 +106,65 @@ public class AgenticSearchQueryIT extends BaseNeuralSearchIT {
         // TODO Should succeed because the hardcoded DSL query is executed
         Response response = client().performRequest(searchRequest);
         assertEquals(200, response.getStatusLine().getStatusCode());
+    }
+
+    public void testAgenticSearchQuery_withSingleShard_thenSuccess() throws Exception {
+        String singleShardIndex = TEST_INDEX + "-single-shard";
+        initializeIndexIfNotExist(singleShardIndex, 1);
+
+        String queryJson = String.format(
+            Locale.ROOT,
+            "{" + "\"query\": {" + "\"agentic_search\": {" + "\"query_text\": \"%s\"," + "\"agent_id\": \"%s\"" + "}" + "}" + "}",
+            TEST_QUERY_TEXT,
+            TEST_AGENT_ID
+        );
+
+        Request searchRequest = new Request("POST", "/" + singleShardIndex + "/_search");
+        searchRequest.setJsonEntity(queryJson);
+
+        Response response = client().performRequest(searchRequest);
+        assertEquals(200, response.getStatusLine().getStatusCode());
+    }
+
+    public void testAgenticSearchQuery_withMultipleShards_thenSuccess() throws Exception {
+        String multiShardIndex = TEST_INDEX + "-multi-shard";
+        initializeIndexIfNotExist(multiShardIndex, 3);
+
+        String queryJson = String.format(
+            Locale.ROOT,
+            "{" + "\"query\": {" + "\"agentic_search\": {" + "\"query_text\": \"%s\"," + "\"agent_id\": \"%s\"" + "}" + "}" + "}",
+            TEST_QUERY_TEXT,
+            TEST_AGENT_ID
+        );
+
+        Request searchRequest = new Request("POST", "/" + multiShardIndex + "/_search");
+        searchRequest.setJsonEntity(queryJson);
+
+        Response response = client().performRequest(searchRequest);
+        assertEquals(200, response.getStatusLine().getStatusCode());
+    }
+
+    private void initializeIndexIfNotExist(String indexName) throws Exception {
+        initializeIndexIfNotExist(indexName, 1);
+    }
+
+    private void initializeIndexIfNotExist(String indexName, int numberOfShards) throws Exception {
+        if (!indexExists(indexName)) {
+            createIndexWithConfiguration(
+                indexName,
+                buildIndexConfiguration(
+                    Collections.emptyList(),
+                    Map.of(),
+                    Collections.emptyList(),
+                    Collections.singletonList("passage_text"),
+                    Collections.emptyList(),
+                    numberOfShards
+                ),
+                ""
+            );
+            addDocument(indexName, "1", "passage_text", "This is about science and technology", null, null);
+            addDocument(indexName, "2", "passage_text", "Machine learning and artificial intelligence", null, null);
+            assertEquals(2, getDocCount(indexName));
+        }
     }
 }
