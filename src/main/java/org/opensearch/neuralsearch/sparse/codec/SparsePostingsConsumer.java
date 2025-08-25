1aa97fa0ee858fdef63cc4fc95f87abbb239a536
yuye-aws <yuyezhu@amazon.com>
2025-08-25 14:01:28 +0800

    simplify SparsePostingsConsumer
    Signed-off-by: yuye-aws <yuyezhu@amazon.com>

--- src/main/java/org/opensearch/neuralsearch/sparse/codec/SparsePostingsConsumer.java
+++ src/main/java/org/opensearch/neuralsearch/sparse/codec/SparsePostingsConsumer.java
@@ -4,17 +4,12 @@
  */
 package org.opensearch.neuralsearch.sparse.codec;
 
-import java.io.IOException;
-import org.apache.lucene.index.Fields;
-import org.apache.lucene.codecs.NormsProducer;
 import lombok.extern.log4j.Log4j2;
-import org.apache.lucene.codecs.FieldsConsumer;
 
 /**
  * This class is responsible for writing sparse postings to the index
  */
-@Log4j2
-public class SparsePostingsConsumer extends FieldsConsumer {
+public class SparsePostingsConsumer {
 
     static final String CODEC_NAME = "SparsePostingsProducer";
 
@@ -25,14 +20,4 @@ public class SparsePostingsConsumer extends FieldsConsumer {
     /** Extension of terms file */
     static final String TERMS_EXTENSION = "sit";
     static final String POSTING_EXTENSION = "sip";
-
-    @Override
-    public void write(Fields fields, NormsProducer norms) throws IOException {
-        // TODO: implement the write method
-    }
-
-    @Override
-    public void close() throws IOException {
-        // TODO: implement the close method
-    }
 }
