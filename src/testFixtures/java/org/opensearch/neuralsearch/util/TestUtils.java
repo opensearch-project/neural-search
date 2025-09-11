05038c63db329716e084e2f662728fc2cec51331
Junqiu Lei <junqiu@amazon.com>
2025-09-10 17:25:23 -0700

    Add integration test with remote model by local hosted torch server
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

--- src/testFixtures/java/org/opensearch/neuralsearch/util/TestUtils.java
+++ src/testFixtures/java/org/opensearch/neuralsearch/util/TestUtils.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
