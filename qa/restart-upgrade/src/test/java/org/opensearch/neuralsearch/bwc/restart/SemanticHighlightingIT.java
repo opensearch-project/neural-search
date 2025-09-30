65b14669df5f35a30aa020b916bf5079aa4829b6
Junqiu Lei <junqiu@amazon.com>
2025-09-30 13:24:23 -0700

    fix bwc failure
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

--- qa/restart-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/restart/SemanticHighlightingIT.java
+++ qa/restart-upgrade/src/test/java/org/opensearch/neuralsearch/bwc/restart/SemanticHighlightingIT.java
@@ -108,7 +108,20 @@ public class SemanticHighlightingIT extends AbstractRestartUpgradeRestTestCase {
             verifyHighlightingWithNewDocument(highlightModelId);
 
         } finally {
-            wipeOfTestResources(TEST_INDEX, EMBEDDING_PIPELINE, highlightModelId, embeddingModelId);
+            // Clean up test resources - no search pipeline in this test
+            try {
+                wipeOfTestResources(TEST_INDEX, EMBEDDING_PIPELINE, embeddingModelId, null);
+            } catch (Exception e) {
+                logger.warn("Error during cleanup, continuing: {}", e.getMessage());
+            }
+            // Manually delete highlight model since wipeOfTestResources only handles one model
+            if (highlightModelId != null) {
+                try {
+                    deleteModel(highlightModelId);
+                } catch (Exception e) {
+                    logger.warn("Error deleting highlight model, continuing: {}", e.getMessage());
+                }
+            }
         }
     }
 
