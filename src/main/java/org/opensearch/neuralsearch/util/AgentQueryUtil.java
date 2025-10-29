9a9b033ee74b63bf6260bebe3b5babbfcb3275c6
Owais Kazi <owaiskazi19@gmail.com>
2025-10-28 19:25:48 -0700

    [Agentic Search] Extract agent summary trace based on model type (#1633)
    * Extract agent summary trace based on model type
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    * Addressed comments
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    
    ---------
    
    Signed-off-by: Owais Kazi <owaiskazi19@gmail.com>
    (cherry picked from commit b9324608715c130dd093525823aa2efd10247365)

---
+++ src/main/java/org/opensearch/neuralsearch/util/AgentQueryUtil.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.opensearch.neuralsearch.util;
+
+/**
+ * Constants for agent-related operations
+ */
+public class AgentQueryUtil {
+    public static final String CLAUDE_MODEL_PREFIX = "claude";
+    public static final String OPENAI_MODEL_PREFIX = "openai";
+
+    private AgentQueryUtil() {}
+}
