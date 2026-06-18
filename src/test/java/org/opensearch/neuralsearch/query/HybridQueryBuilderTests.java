afe764c09cbdce38eff25a540d02b5455ba8d55d
Venkateshwaran Shanmugham <venkateshwaracholan@gmail.com>
2026-06-18 13:30:49 +0530

    Add tests for unsupported hybrid query field parsing paths
    Signed-off-by: Venkateshwaran Shanmugham <venkateshwaracholan@gmail.com>

--- src/test/java/org/opensearch/neuralsearch/query/HybridQueryBuilderTests.java
+++ src/test/java/org/opensearch/neuralsearch/query/HybridQueryBuilderTests.java
@@ -5,6 +5,7 @@
 package org.opensearch.neuralsearch.query;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -661,6 +662,96 @@ public class HybridQueryBuilderTests extends OpenSearchQueryTestCase {
 
         ParsingException exception = expectThrows(ParsingException.class, () -> HybridQueryBuilder.fromXContent(contentParser));
         assertThat(exception.getMessage(), containsString("[hybrid] query's [filter] field must be a query object"));
+    }
+
+    /**
+     * Tests that an unsupported top-level object field returns the generic hybrid error
+     * without echoing the customer-provided field name in the exception message.
+     */
+    @SneakyThrows
+    public void testFromXContent_whenUnsupportedFieldIsObject_thenFailWithGenericMessage() {
+        XContentBuilder xContentBuilder = XContentFactory.jsonBuilder()
+            .startObject()
+            .startArray("queries")
+            .startObject()
+            .startObject(TermQueryBuilder.NAME)
+            .field(TEXT_FIELD_NAME, TERM_QUERY_TEXT)
+            .endObject()
+            .endObject()
+            .endArray()
+            .startObject("random_field")
+            .startObject(TermQueryBuilder.NAME)
+            .field(TEXT_FIELD_NAME, TERM_QUERY_TEXT)
+            .endObject()
+            .endObject()
+            .endObject();
+
+        NamedXContentRegistry namedXContentRegistry = new NamedXContentRegistry(
+            List.of(
+                new NamedXContentRegistry.Entry(QueryBuilder.class, new ParseField(TermQueryBuilder.NAME), TermQueryBuilder::fromXContent),
+                new NamedXContentRegistry.Entry(
+                    QueryBuilder.class,
+                    new ParseField(HybridQueryBuilder.NAME),
+                    HybridQueryBuilder::fromXContent
+                )
+            )
+        );
+        XContentParser contentParser = createParser(
+            namedXContentRegistry,
+            xContentBuilder.contentType().xContent(),
+            BytesReference.bytes(xContentBuilder)
+        );
+        contentParser.nextToken();
+
+        ParsingException exception = expectThrows(ParsingException.class, () -> HybridQueryBuilder.fromXContent(contentParser));
+        assertThat(exception.getMessage(), containsString("Field is not supported by [hybrid] query"));
+        assertThat(exception.getMessage(), not(containsString("random_field")));
+    }
+
+    /**
+     * Tests that an unsupported top-level array field returns the generic hybrid error
+     * without echoing the customer-provided field name in the exception message.
+     */
+    @SneakyThrows
+    public void testFromXContent_whenUnsupportedFieldIsArray_thenFailWithGenericMessage() {
+        XContentBuilder xContentBuilder = XContentFactory.jsonBuilder()
+            .startObject()
+            .startArray("queries")
+            .startObject()
+            .startObject(TermQueryBuilder.NAME)
+            .field(TEXT_FIELD_NAME, TERM_QUERY_TEXT)
+            .endObject()
+            .endObject()
+            .endArray()
+            .startArray("random_field")
+            .startObject()
+            .startObject(TermQueryBuilder.NAME)
+            .field(TEXT_FIELD_NAME, TERM_QUERY_TEXT)
+            .endObject()
+            .endObject()
+            .endArray()
+            .endObject();
+
+        NamedXContentRegistry namedXContentRegistry = new NamedXContentRegistry(
+            List.of(
+                new NamedXContentRegistry.Entry(QueryBuilder.class, new ParseField(TermQueryBuilder.NAME), TermQueryBuilder::fromXContent),
+                new NamedXContentRegistry.Entry(
+                    QueryBuilder.class,
+                    new ParseField(HybridQueryBuilder.NAME),
+                    HybridQueryBuilder::fromXContent
+                )
+            )
+        );
+        XContentParser contentParser = createParser(
+            namedXContentRegistry,
+            xContentBuilder.contentType().xContent(),
+            BytesReference.bytes(xContentBuilder)
+        );
+        contentParser.nextToken();
+
+        ParsingException exception = expectThrows(ParsingException.class, () -> HybridQueryBuilder.fromXContent(contentParser));
+        assertThat(exception.getMessage(), containsString("Field is not supported by [hybrid] query"));
+        assertThat(exception.getMessage(), not(containsString("random_field")));
     }
 
     @SneakyThrows
